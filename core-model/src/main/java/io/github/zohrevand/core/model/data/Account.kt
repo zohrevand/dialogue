da4f7c02581e840458d54b9d605174f92b997170
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-06 20:08:34 +0430

    Add core-model module and Account model

---
+++ core-model/src/main/java/io/github/zohrevand/core/model/data/Account.kt
@@ -0,0 +1,24 @@
+package io.github.zohrevand.core.model.data
+
+data class Account(
+    val id: String,
+    val username: String,
+    val domain: String,
+    val password: String,
+    val displayName: String,
+    val resource: String,
+    val status: Status
+) {
+    enum class Status {
+        Registering,
+        InLogin,
+        Disabled,
+        Offline,
+        Connecting,
+        Online,
+        Unauthorized,
+        ServerNotFound,
+        RegistrationSuccessful,
+        RegistrationFailed
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "dialogue"
 include(":app")
+include(":core-model")
