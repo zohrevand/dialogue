a844cdaef693c1e44c6fa400c74ac6bf1a00984e
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-08 21:43:33 +0430

    Configure Smack initializer in DialogueApp

--- app/src/main/java/io/github/zohrevand/dialogue/DialogueApp.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/DialogueApp.kt
@@ -2,7 +2,13 @@ package io.github.zohrevand.dialogue
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.jivesoftware.smack.android.AndroidSmackInitializer
 
 @HiltAndroidApp
 class DialogueApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AndroidSmackInitializer.initialize(this)
+    }
 }
