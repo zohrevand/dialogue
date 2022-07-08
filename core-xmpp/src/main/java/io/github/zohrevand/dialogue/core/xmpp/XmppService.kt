5647c6b1b2377be141be8e7a7139815bed30196c
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-08 20:51:17 +0430

    Add XmppService, a service that manage XMPP connection

---
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppService.kt
@@ -0,0 +1,10 @@
+package io.github.zohrevand.dialogue.core.xmpp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class XmppService : Service() {
+
+    override fun onBind(p0: Intent?): IBinder? = null
+}
