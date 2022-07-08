48baa2c2e153280c17aef80c2e9363b3d1421b72
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 00:58:53 +0430

    Add xmppTcpConnection to XmppManager and implementation

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManager.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManager.kt
@@ -1,4 +1,8 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+
 interface XmppManager {
+
+    suspend fun getConnection(): XMPPTCPConnection
 }
--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
@@ -1,6 +1,12 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import javax.inject.Inject
 
 class XmppManagerImpl @Inject constructor() : XmppManager {
+
+    private var xmppConnection: XMPPTCPConnection? = null
+
+    override suspend fun getConnection(): XMPPTCPConnection =
+        xmppConnection ?: throw NoSuchElementException("Connection is not established.")
 }
