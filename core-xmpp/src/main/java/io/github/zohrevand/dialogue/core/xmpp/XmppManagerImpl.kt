804c93790a4101d8a6ed20f342b4cc1f6ee77f87
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 01:01:12 +0430

    Implement getConfiguration function

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
@@ -1,6 +1,8 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import io.github.zohrevand.core.model.data.Account
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
 import javax.inject.Inject
 
 class XmppManagerImpl @Inject constructor() : XmppManager {
@@ -10,3 +12,9 @@ class XmppManagerImpl @Inject constructor() : XmppManager {
     override suspend fun getConnection(): XMPPTCPConnection =
         xmppConnection ?: throw NoSuchElementException("Connection is not established.")
 }
+
+private fun getConfiguration(account: Account): XMPPTCPConnectionConfiguration =
+    XMPPTCPConnectionConfiguration.builder()
+        .setUsernameAndPassword(account.username, account.password)
+        .setXmppDomain(account.domain)
+        .build()
