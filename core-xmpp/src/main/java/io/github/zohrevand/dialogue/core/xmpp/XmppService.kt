49fc9c72b8c48a67ee466943a026aeec526f7cdf
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 15:45:56 +0430

    Initiate XMPP login from XmppService

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppService.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppService.kt
@@ -4,13 +4,43 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.zohrevand.dialogue.core.xmpp.collector.AccountsCollector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class XmppService : Service() {
 
+    private val scope = CoroutineScope(SupervisorJob())
+
     @Inject
     lateinit var xmppManager: XmppManager
 
+    @Inject
+    lateinit var accountsCollector: AccountsCollector
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        observeAccountsStream()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun observeAccountsStream() {
+        scope.launch {
+            accountsCollector.collectAccounts(
+                onNewLogin = { scope.launch { xmppManager.login(it) } },
+            )
+        }
+    }
+
     override fun onBind(p0: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        scope.cancel()
+        super.onDestroy()
+    }
 }
