e476f87b9a4a94b7c5214a0ecc8dcc3ca135bacd
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 16:53:47 +0430

    Configure XMPP ReconnectionManager

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
@@ -3,6 +3,7 @@ package io.github.zohrevand.dialogue.core.xmpp
 import io.github.zohrevand.core.model.data.Account
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import org.jivesoftware.smack.ReconnectionManager
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
 import javax.inject.Inject
@@ -19,7 +20,8 @@ class XmppManagerImpl @Inject constructor(
     override suspend fun login(account: Account) {
         xmppConnection = account.login(
             configurationBuilder = ::getConfiguration,
-            connectionBuilder = ::XMPPTCPConnection
+            connectionBuilder = ::XMPPTCPConnection,
+            reconnectionManager = ::configureReconnectionManager
         )
     }
 
@@ -30,6 +32,7 @@ class XmppManagerImpl @Inject constructor(
     private suspend fun Account.login(
         configurationBuilder: (Account) -> XMPPTCPConnectionConfiguration,
         connectionBuilder: (XMPPTCPConnectionConfiguration) -> XMPPTCPConnection,
+        reconnectionManager: (XMPPTCPConnection) -> Unit
     ): XMPPTCPConnection {
         val configuration = configurationBuilder(this)
         val connection = connectionBuilder(configuration)
@@ -51,4 +54,9 @@ class XmppManagerImpl @Inject constructor(
             login()
             this@connectAndLogin
         }
+
+    private fun configureReconnectionManager(connection: XMPPTCPConnection) {
+        ReconnectionManager.getInstanceFor(connection)
+            .enableAutomaticReconnection()
+    }
 }
