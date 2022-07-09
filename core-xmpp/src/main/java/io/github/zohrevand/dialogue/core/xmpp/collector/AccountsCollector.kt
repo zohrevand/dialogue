b4384b4582d5caeb8c12a89bf3c94bd1f343fdf0
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 15:44:53 +0430

    Collect accountsStream and check for PreLoggingIn status

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/AccountsCollector.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/AccountsCollector.kt
@@ -1,8 +1,12 @@
 package io.github.zohrevand.dialogue.core.xmpp.collector
 
+import io.github.zohrevand.core.model.data.Account
+
 interface AccountsCollector {
     /**
     * Collects the changes to accounts stream originated from database
     * */
-    fun collectAccounts()
+    suspend fun collectAccounts(
+        onNewLogin: (Account) -> Unit,
+    )
 }
--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/AccountsCollectorImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/AccountsCollectorImpl.kt
@@ -1,13 +1,26 @@
 package io.github.zohrevand.dialogue.core.xmpp.collector
 
+import io.github.zohrevand.core.model.data.Account
+import io.github.zohrevand.core.model.data.AccountStatus.LoggingIn
+import io.github.zohrevand.core.model.data.AccountStatus.PreLoggingIn
 import io.github.zohrevand.dialogue.core.data.repository.AccountsRepository
+import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
 
 class AccountsCollectorImpl @Inject constructor(
     private val accountsRepository: AccountsRepository
 ) : AccountsCollector {
 
-    override fun collectAccounts() {
-        TODO("Not yet implemented")
+    override suspend fun collectAccounts(
+        onNewLogin: (Account) -> Unit,
+    ) {
+        accountsRepository.getAccountsStream().collectLatest { accounts ->
+            val preLoggingInAccount = accounts.firstOrNull { it.status == PreLoggingIn }
+            preLoggingInAccount?.let {
+                onNewLogin(it)
+                accountsRepository.updateAccount(it.copy(status = LoggingIn))
+            }
+
+        }
     }
 }
