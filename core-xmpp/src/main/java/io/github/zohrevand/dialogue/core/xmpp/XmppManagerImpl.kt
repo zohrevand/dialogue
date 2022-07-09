5cadfada7d1d17bdd31d136171dda8c9bcdf935a
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-09 13:58:17 +0430

    Moving global functions to XmppManagerImpl

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppManagerImpl.kt
@@ -1,6 +1,7 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
 import io.github.zohrevand.core.model.data.Account
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
@@ -20,31 +21,31 @@ class XmppManagerImpl @Inject constructor() : XmppManager {
             connectionBuilder = ::XMPPTCPConnection
         )
     }
-}
 
-private suspend fun Account.login(
-    configurationBuilder: (Account) -> XMPPTCPConnectionConfiguration,
-    connectionBuilder: (XMPPTCPConnectionConfiguration) -> XMPPTCPConnection,
-): XMPPTCPConnection {
-    val configuration = configurationBuilder(this)
-    val connection = connectionBuilder(configuration)
+    private suspend fun Account.login(
+        configurationBuilder: (Account) -> XMPPTCPConnectionConfiguration,
+        connectionBuilder: (XMPPTCPConnectionConfiguration) -> XMPPTCPConnection,
+    ): XMPPTCPConnection {
+        val configuration = configurationBuilder(this)
+        val connection = connectionBuilder(configuration)
 
-    return withContext(Dispatchers.IO) {
-        return@withContext connection.connectAndLogin()
+        return withContext(Dispatchers.IO) {
+            return@withContext connection.connectAndLogin()
+        }
     }
-}
 
-private fun getConfiguration(account: Account): XMPPTCPConnectionConfiguration =
-    XMPPTCPConnectionConfiguration.builder()
-        .setUsernameAndPassword(account.username, account.password)
-        .setXmppDomain(account.domain)
-        .build()
+    private fun getConfiguration(account: Account): XMPPTCPConnectionConfiguration =
+        XMPPTCPConnectionConfiguration.builder()
+            .setUsernameAndPassword(account.username, account.password)
+            .setXmppDomain(account.domain)
+            .build()
 
-/**
-* This call blocks
-* */
-private fun XMPPTCPConnection.connectAndLogin(): XMPPTCPConnection {
-    connect()
-    login()
-    return this
+    /**
+     * This call blocks
+     * */
+    private fun XMPPTCPConnection.connectAndLogin(): XMPPTCPConnection {
+        connect()
+        login()
+        return this
+    }
 }
