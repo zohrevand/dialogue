b49155f637e2ecd25d5aaeda3dfd3cf68ba071b1
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-10 20:35:39 +0430

    Turn off the decor fitting system windows

--- app/src/main/java/io/github/zohrevand/dialogue/MainActivity.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.zohrevand.dialogue.core.xmpp.XmppService
 import io.github.zohrevand.dialogue.ui.DialogueApp
@@ -15,6 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         startService(Intent(this, XmppService::class.java))
+
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent { DialogueApp() }
     }
