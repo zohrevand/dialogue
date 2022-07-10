f86c30390fb4829cccac21d542aea7bf8ec9fe1f
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-10 21:57:31 +0430

    Add initial scaffolding and navigation

---
+++ app/src/main/java/io/github/zohrevand/dialogue/navigation/DialogueNavHost.kt
@@ -0,0 +1,14 @@
+package io.github.zohrevand.dialogue.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DialogueNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+
+}
--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueApp.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueApp.kt
@@ -1,8 +1,28 @@
 package io.github.zohrevand.dialogue.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.rememberNavController
+import io.github.zohrevand.dialogue.navigation.DialogueNavHost
+import io.github.zohrevand.dialogue.ui.theme.DialogueTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DialogueApp() {
+    DialogueTheme {
+        val navController = rememberNavController()
 
+        Scaffold(
+            containerColor = Color.Transparent
+        ) { padding ->
+            DialogueNavHost(
+                navController = navController,
+                modifier = Modifier.padding(padding)
+            )
+        }
+    }
 }
