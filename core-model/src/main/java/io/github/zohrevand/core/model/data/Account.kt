c9d2933d8444358028fdbe4ddb2ee176ca558461
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-11 13:42:24 +0430

    Add factory method to Account and helper for parsing JID

--- core-model/src/main/java/io/github/zohrevand/core/model/data/Account.kt
+++ core-model/src/main/java/io/github/zohrevand/core/model/data/Account.kt
@@ -1,5 +1,7 @@
 package io.github.zohrevand.core.model.data
 
+import io.github.zohrevand.core.model.data.AccountStatus.PreLoggingIn
+
 data class Account(
     val id: Long? = null,
     val username: String,
@@ -8,4 +10,27 @@ data class Account(
     val displayName: String,
     val resource: String,
     val status: AccountStatus
-)
+) {
+    companion object {
+        fun create(jid: String, password: String): Account {
+            val (username, domain) = jid.usernameDomain
+            return Account(
+                username = username,
+                domain = domain,
+                password = password,
+                status = PreLoggingIn,
+                // TODO(): ignore for now
+                displayName = "",
+                // TODO(): ignore for now
+                resource = "",
+            )
+        }
+    }
+}
+
+// TODO(): consider jid comprises only local part and domain part for now
+private val String.usernameDomain: Pair<String, String>
+    get() {
+        val (username, domain) = this.split("@")
+        return Pair(username, domain)
+    }
