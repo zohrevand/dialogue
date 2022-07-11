6af076f23fd8c7b3dfd83a38174dd5679494d8e7
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-11 13:48:40 +0430

    Add basic login UI to AuthScreen, connect to hiltViewModel

--- feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthScreen.kt
+++ feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthScreen.kt
@@ -1,38 +1,86 @@
 package io.github.zohrevand.dialogue.feature.auth
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun AuthRoute(
     navigateToConversations: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: AuthViewModel = hiltViewModel()
 ) {
     AuthScreen(
-        navigateToConversations = navigateToConversations,
+        onLoginClick = viewModel::login,
         modifier = modifier
     )
 }
 
 @Composable
 fun AuthScreen(
-    navigateToConversations: () -> Unit,
+    onLoginClick: (String, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val (jid, setJid) = remember { mutableStateOf("") }
+    val (password, setPassword) = remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Red),
+            .safeContentPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
-        Text(text = "This is Login Screen")
-        Button(onClick = navigateToConversations) {
-            Text(text = "Navigate to conversations")
+        Text(text = "Enter your login information")
+
+        OutlinedTextField(
+            value = jid,
+            onValueChange = setJid,
+            label = { Text(text = "Jabber ID") },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = setPassword,
+            label = { Text(text = "Password") },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            modifier = modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = { onLoginClick(jid, password) },
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(text = "Login")
         }
     }
 }
