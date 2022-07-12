e77fe020b1f1e526315482829519447aa179bb5d
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-12 21:19:08 +0430

    Add uiState to AuthViewModel

--- feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthViewModel.kt
+++ feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.zohrevand.core.model.data.Account
 import io.github.zohrevand.dialogue.core.data.repository.AccountsRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,11 +17,15 @@ class AuthViewModel @Inject constructor(
     private val accountsRepository: AccountsRepository
 ) : ViewModel() {
 
+    private val _uiState: MutableStateFlow<AuthUiState> = MutableStateFlow(AuthUiState.Idle)
+    val uiState: StateFlow<AuthUiState> = _uiState.asStateFlow()
+
     private var authInputState: AuthInputState? = null
 
     fun login(jid: String, password: String) {
         authInputState = AuthInputState(jid)
         val account = Account.create(jid, password)
+        _uiState.update { AuthUiState.Loading }
         viewModelScope.launch {
             accountsRepository.deleteAllAccounts()
             accountsRepository.addAccount(account)
