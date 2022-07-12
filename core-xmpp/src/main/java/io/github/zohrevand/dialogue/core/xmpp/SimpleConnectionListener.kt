0ddb521c54649845b93ff14ec7f18981cd63a3b4
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-12 20:42:44 +0430

    Add SimpleConnectionListener

---
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/SimpleConnectionListener.kt
@@ -0,0 +1,29 @@
+package io.github.zohrevand.dialogue.core.xmpp
+
+import android.util.Log
+import org.jivesoftware.smack.ConnectionListener
+import org.jivesoftware.smack.XMPPConnection
+
+private const val TAG = "ConnectionListener"
+
+class SimpleConnectionListener : ConnectionListener {
+    override fun connecting(connection: XMPPConnection?) {
+        Log.d(TAG, "connecting... (ConnectionListener)")
+    }
+
+    override fun connected(connection: XMPPConnection?) {
+        Log.d(TAG, "connected (ConnectionListener)")
+    }
+
+    override fun authenticated(connection: XMPPConnection?, resumed: Boolean) {
+        Log.d(TAG, "authenticated (ConnectionListener), resumed: $resumed")
+    }
+
+    override fun connectionClosed() {
+        Log.d(TAG, "connectionClosed (ConnectionListener)")
+    }
+
+    override fun connectionClosedOnError(e: Exception?) {
+        Log.d(TAG, "connectionClosedOnError (ConnectionListener) with error: $e")
+    }
+}
