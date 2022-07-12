da738ec46dfe2759aeae033b9d9840c80ed0bdc8
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-12 22:01:02 +0430

    Check for account status changes in AuthViewModel

--- feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthViewModel.kt
+++ feature-auth/src/main/java/io/github/zohrevand/dialogue/feature/auth/AuthViewModel.kt
@@ -4,10 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.zohrevand.core.model.data.Account
+import io.github.zohrevand.core.model.data.AccountStatus.LoggingIn
+import io.github.zohrevand.core.model.data.AccountStatus.Online
+import io.github.zohrevand.core.model.data.AccountStatus.ServerNotFound
+import io.github.zohrevand.core.model.data.AccountStatus.Unauthorized
+import io.github.zohrevand.core.model.data.usernameDomain
 import io.github.zohrevand.dialogue.core.data.repository.AccountsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,22 +28,41 @@ class AuthViewModel @Inject constructor(
     private val _uiState: MutableStateFlow<AuthUiState> = MutableStateFlow(AuthUiState.Idle)
     val uiState: StateFlow<AuthUiState> = _uiState.asStateFlow()
 
-    private var authInputState: AuthInputState? = null
-
     fun login(jid: String, password: String) {
-        authInputState = AuthInputState(jid)
         val account = Account.create(jid, password)
         _uiState.update { AuthUiState.Loading }
         viewModelScope.launch {
             accountsRepository.deleteAllAccounts()
             accountsRepository.addAccount(account)
+
+            checkForAccountStatusChanges(jid)
         }
     }
-}
 
-data class AuthInputState(
-    val jid: String
-)
+    private suspend fun checkForAccountStatusChanges(jid: String) {
+        val usernameDomain = jid.usernameDomain
+        val username = usernameDomain.first
+        val domain = usernameDomain.second
+
+        accountsRepository.getAccount(
+            username = username,
+            domain = domain
+        )
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = null
+            ).collectLatest { account ->
+                when (account?.status) {
+                    Online -> _uiState.update { AuthUiState.Success }
+                    ServerNotFound -> _uiState.update { AuthUiState.Error("Server not available") }
+                    Unauthorized -> _uiState.update { AuthUiState.Error("You are not authorized") }
+                    else -> { /*Not interested*/
+                    }
+                }
+            }
+    }
+}
 
 sealed interface AuthUiState {
     object Idle : AuthUiState
