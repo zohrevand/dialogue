3025b2672c251709b87be04b40c9d0c0aff8f227
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-14 23:52:43 +0430

    Update ConnectionStatus in datastore

--- core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/DialoguePreferencesDataSource.kt
+++ core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/DialoguePreferencesDataSource.kt
@@ -1,8 +1,10 @@
 package io.github.zohrevand.dialogue.core.datastore
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
 class DialoguePreferencesDataSource @Inject constructor(
@@ -13,8 +15,31 @@ class DialoguePreferencesDataSource @Inject constructor(
         .map {
             ConnectionStatus(
                 availability = it.connectionAvailability,
-                unauthorized = it.connectionAuthorized,
+                unauthorized = it.connectionAuthorized
             )
         }
         .firstOrNull() ?: ConnectionStatus()
+
+    /**
+     * Update the [ConnectionStatus] using [update].
+     */
+    suspend fun updateConnectionStatus(update: ConnectionStatus.() -> ConnectionStatus) {
+        try {
+            userPreferences.updateData { currentPreferences ->
+                val updatedConnectionStatus = update(
+                    ConnectionStatus(
+                        availability = currentPreferences.connectionAvailability,
+                        unauthorized = currentPreferences.connectionAuthorized
+                    )
+                )
+
+                currentPreferences.copy {
+                    connectionAvailability = updatedConnectionStatus.availability
+                    connectionAuthorized = updatedConnectionStatus.unauthorized
+                }
+            }
+        } catch (ioException: IOException) {
+            Log.e("NiaPreferences", "Failed to update user preferences", ioException)
+        }
+    }
 }
