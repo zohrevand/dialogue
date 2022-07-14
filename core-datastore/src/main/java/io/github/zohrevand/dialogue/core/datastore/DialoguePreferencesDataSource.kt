5eb0757faa8affe931eab5adac404b394acb41f7
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-14 23:50:10 +0430

    Get ConnectionStatus from datastore

--- core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/DialoguePreferencesDataSource.kt
+++ core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/DialoguePreferencesDataSource.kt
@@ -1,10 +1,20 @@
 package io.github.zohrevand.dialogue.core.datastore
 
 import androidx.datastore.core.DataStore
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class DialoguePreferencesDataSource @Inject constructor(
     private val userPreferences: DataStore<UserPreferences>
 ) {
 
+    suspend fun getConnectionStatus() = userPreferences.data
+        .map {
+            ConnectionStatus(
+                availability = it.connectionAvailability,
+                unauthorized = it.connectionAuthorized,
+            )
+        }
+        .firstOrNull() ?: ConnectionStatus()
 }
