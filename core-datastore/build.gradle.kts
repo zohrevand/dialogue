e1b21a4dda3c38e77a018f0cd14a10e048d01de2
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-14 23:13:38 +0430

    Setup protobuf configuration

--- core-datastore/build.gradle.kts
+++ core-datastore/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.google.protobuf.gradle.builtins
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
 // TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -11,6 +16,25 @@ plugins {
 android {
     defaultConfig {
         consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+}
+
+// Setup protobuf configuration
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                val java by registering {
+                    option("lite")
+                }
+                val kotlin by registering {
+                    option("lite")
+                }
+            }
+        }
     }
 }
 
