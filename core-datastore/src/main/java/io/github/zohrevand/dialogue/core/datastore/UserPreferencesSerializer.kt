23b1c5671fdf9e380047e3a22f3a2392330de897
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-14 23:45:23 +0430

    Add UserPreferencesSerializer

---
+++ core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/UserPreferencesSerializer.kt
@@ -0,0 +1,30 @@
+package io.github.zohrevand.dialogue.core.datastore
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+import javax.inject.Inject
+
+/**
+ * An [androidx.datastore.core.Serializer] for the [UserPreferences] proto.
+ */
+class UserPreferencesSerializer @Inject constructor() : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences =
+        try {
+            // readFrom is already called on the data store background thread
+            @Suppress("BlockingMethodInNonBlockingContext")
+            UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+
+    override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
+        // writeTo is already called on the data store background thread
+        @Suppress("BlockingMethodInNonBlockingContext")
+        t.writeTo(output)
+    }
+}
