c826223317ce12a5514c2f4b0b74d84e59038291
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-17 00:44:09 +0430

    Make XmppService observe connectionStatus and account in parallel

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppService.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/XmppService.kt
@@ -34,7 +34,13 @@ class XmppService : Service() {
 
         scope.launch {
             initializeXmppManager()
+        }
+
+        scope.launch {
             observeConnectionStatus()
+        }
+
+        scope.launch {
             observeAccountsStream()
         }
 
