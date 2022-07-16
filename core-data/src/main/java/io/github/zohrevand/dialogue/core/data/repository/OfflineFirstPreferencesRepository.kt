25588b6bb81479199ed85761a79f492f8ee8c67c
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-16 23:14:39 +0430

    Add getAccount implementation

--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstPreferencesRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstPreferencesRepository.kt
@@ -3,7 +3,9 @@ package io.github.zohrevand.dialogue.core.data.repository
 import io.github.zohrevand.core.model.data.Account
 import io.github.zohrevand.dialogue.core.datastore.ConnectionStatus
 import io.github.zohrevand.dialogue.core.datastore.DialoguePreferencesDataSource
+import io.github.zohrevand.dialogue.core.datastore.asExternalModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class OfflineFirstPreferencesRepository @Inject constructor(
@@ -16,9 +18,8 @@ class OfflineFirstPreferencesRepository @Inject constructor(
     override suspend fun updateConnectionStatus(connectionStatus: ConnectionStatus) =
         preferencesDataSource.updateConnectionStatus(connectionStatus)
 
-    override fun getAccount(): Flow<Account> {
-        TODO("Not yet implemented")
-    }
+    override fun getAccount(): Flow<Account> =
+        preferencesDataSource.getAccount().map { it.asExternalModel() }
 
     override suspend fun updateAccount(account: Account) {
         TODO("Not yet implemented")
