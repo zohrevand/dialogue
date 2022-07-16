d8dfbdbbb7b89c580e8b7645df2aaa0645e75b18
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-16 22:53:06 +0430

    Change Account model completely

--- core-model/src/main/java/io/github/zohrevand/core/model/data/Account.kt
+++ core-model/src/main/java/io/github/zohrevand/core/model/data/Account.kt
@@ -3,34 +3,29 @@ package io.github.zohrevand.core.model.data
 import io.github.zohrevand.core.model.data.AccountStatus.PreLoggingIn
 
 data class Account(
-    val id: Long? = null,
-    val username: String,
-    val domain: String,
+    val jid: String,
+    val localPart: String,
+    val domainPart: String,
     val password: String,
-    val displayName: String,
-    val resource: String,
     val status: AccountStatus
 ) {
     companion object {
         fun create(jid: String, password: String): Account {
-            val (username, domain) = jid.usernameDomain
+            val (localPart, domainPart) = jid.localPartDomainPart
             return Account(
-                username = username,
-                domain = domain,
+                jid = jid,
+                localPart = localPart,
+                domainPart = domainPart,
                 password = password,
-                status = PreLoggingIn,
-                // TODO(): ignore for now
-                displayName = "",
-                // TODO(): ignore for now
-                resource = "",
+                status = PreLoggingIn
             )
         }
     }
 }
 
 // TODO(): consider jid comprises only local part and domain part for now
-val String.usernameDomain: Pair<String, String>
+private val String.localPartDomainPart: Pair<String, String>
     get() {
-        val (username, domain) = this.split("@")
-        return Pair(username, domain)
+        val (localPart, domainPart) = this.split("@")
+        return Pair(localPart, domainPart)
     }
