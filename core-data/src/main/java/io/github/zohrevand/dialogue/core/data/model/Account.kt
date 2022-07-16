812681083b5bf262874384ac74057e63a9808b8b
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-16 23:19:15 +0430

    Remove Account and related files from core-database

--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/model/Account.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/model/Account.kt
@@ -1,21 +1,8 @@
 package io.github.zohrevand.dialogue.core.data.model
 
 import io.github.zohrevand.core.model.data.Account
-import io.github.zohrevand.core.model.data.AccountStatus.Disabled
-import io.github.zohrevand.dialogue.core.database.model.AccountEntity
 import io.github.zohrevand.dialogue.core.datastore.PreferencesAccount
 import io.github.zohrevand.dialogue.core.datastore.UserPreferences
-
-// TODO: this should be removed
-fun Account.asEntity() = AccountEntity(
-    id = 0,
-    username = "",
-    domain = "",
-    password = "",
-    displayName = "",
-    resource = "",
-    status = Disabled
-)
 
 fun Account.asPreferences() = PreferencesAccount(
     jid = jid,
--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/DialogueDatabase.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/DialogueDatabase.kt
@@ -2,16 +2,12 @@ package io.github.zohrevand.dialogue.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import io.github.zohrevand.dialogue.core.database.dao.AccountDao
-import io.github.zohrevand.dialogue.core.database.model.AccountEntity
 
 @Database(
-    entities = [
-        AccountEntity::class
-    ],
+    entities = [],
     version = 1,
     exportSchema = true
 )
 abstract class DialogueDatabase : RoomDatabase() {
-    abstract fun accountDao(): AccountDao
+
 }
