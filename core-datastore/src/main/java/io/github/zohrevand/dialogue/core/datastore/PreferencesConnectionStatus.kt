f647e03470b53ec491aa3b93da5c570f94fd392a
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-16 23:26:36 +0430

    Add PreferencesConnectionStatus mapper to ConnectionStatus

--- core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/PreferencesConnectionStatus.kt
+++ core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/PreferencesConnectionStatus.kt
@@ -1,6 +1,13 @@
 package io.github.zohrevand.dialogue.core.datastore
 
+import io.github.zohrevand.core.model.data.ConnectionStatus
+
 data class PreferencesConnectionStatus(
     val availability: Boolean = false,
     val authorized: Boolean = false
+)
+
+fun PreferencesConnectionStatus.asExternalModel() = ConnectionStatus(
+    availability = availability,
+    authorized = authorized
 )
