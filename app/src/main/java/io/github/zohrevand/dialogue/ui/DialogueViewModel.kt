412c226580168fafe27dd253adf00369edfcdd70
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-17 21:47:35 +0430

    Add observeConnectionStatus method to DialogueViewModel

--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueViewModel.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueViewModel.kt
@@ -1,14 +1,51 @@
 package io.github.zohrevand.dialogue.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.zohrevand.dialogue.core.data.repository.PreferencesRepository
+import io.github.zohrevand.dialogue.ui.ConnectionUiState.Connected
+import io.github.zohrevand.dialogue.ui.ConnectionUiState.Connecting
+import io.github.zohrevand.dialogue.ui.ConnectionUiState.Idle
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DialogueViewModel @Inject constructor(
     private val preferencesRepository: PreferencesRepository
 ) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<ConnectionUiState> = MutableStateFlow(Idle)
+    val uiState: StateFlow<ConnectionUiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            observeConnectionStatus()
+        }
+    }
+
+    private suspend fun observeConnectionStatus() {
+        preferencesRepository.getConnectionStatus()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = null
+            )
+            .collect { connectionStatus ->
+                if (connectionStatus?.availability == true && connectionStatus.authorized) {
+                    _uiState.update { Connected }
+                } else {
+                    // TODO: navigate to auth screen if connection available but not authorized
+                    _uiState.update { Connecting }
+                }
+            }
+    }
 }
 
 sealed interface ConnectionUiState {
