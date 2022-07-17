7e01b1570d5e92f2028fe24f0f2be0c84dad4fd9
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-17 13:58:49 +0430

    Add RouterScreen

---
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterScreen.kt
@@ -0,0 +1,16 @@
+package io.github.zohrevand.dialogue.feature.router
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RouterRoute(
+    navigateToAuth: () -> Unit,
+    navigateToConversations: () -> Unit
+) {
+
+}
+
+@Composable
+fun RouterScreen() {
+
+}
