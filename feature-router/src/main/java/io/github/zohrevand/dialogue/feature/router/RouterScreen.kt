b0d2ec34684692da116505f0a5307d10867f681b
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-17 14:19:34 +0430

    Add checkIfAccountAlreadyExist to decide next route

--- feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterScreen.kt
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterScreen.kt
@@ -37,5 +37,4 @@ fun RouterScreen(
             else -> {}
         }
     }
-
 }
--- feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
@@ -1,12 +1,19 @@
 package io.github.zohrevand.dialogue.feature.router
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.zohrevand.core.model.data.AccountStatus.Online
 import io.github.zohrevand.dialogue.core.data.repository.PreferencesRepository
+import io.github.zohrevand.dialogue.feature.router.RouterUiState.AuthRequired
 import io.github.zohrevand.dialogue.feature.router.RouterUiState.Loading
+import io.github.zohrevand.dialogue.feature.router.RouterUiState.UserAvailable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,6 +24,20 @@ class RouterViewModel @Inject constructor(
     private val _uiState: MutableStateFlow<RouterUiState> = MutableStateFlow(Loading)
     val uiState: StateFlow<RouterUiState> = _uiState.asStateFlow()
 
+    init {
+        checkIfAccountAlreadyExist()
+    }
+
+    private fun checkIfAccountAlreadyExist() {
+        viewModelScope.launch {
+            val account = preferencesRepository.getAccount().firstOrNull()
+            if (account?.status == Online) {
+                _uiState.update { UserAvailable }
+            } else {
+                _uiState.update { AuthRequired }
+            }
+        }
+    }
 }
 
 sealed interface RouterUiState {
