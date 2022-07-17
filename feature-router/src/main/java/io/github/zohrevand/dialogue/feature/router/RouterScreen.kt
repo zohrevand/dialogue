27ab0110130991a797f46094c3d63afe1a7f04ac
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-17 14:16:21 +0430

    Connect RouterScreen to RouterViewModel uiState

--- feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterScreen.kt
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterScreen.kt
@@ -1,16 +1,41 @@
 package io.github.zohrevand.dialogue.feature.router
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import io.github.zohrevand.dialogue.feature.router.RouterUiState.AuthRequired
+import io.github.zohrevand.dialogue.feature.router.RouterUiState.UserAvailable
 
 @Composable
 fun RouterRoute(
     navigateToAuth: () -> Unit,
-    navigateToConversations: () -> Unit
+    navigateToConversations: () -> Unit,
+    viewModel: RouterViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsState()
 
+    RouterScreen(
+        uiState = uiState,
+        navigateToAuth = navigateToAuth,
+        navigateToConversations = navigateToConversations
+    )
 }
 
 @Composable
-fun RouterScreen() {
+fun RouterScreen(
+    uiState: RouterUiState,
+    navigateToAuth: () -> Unit,
+    navigateToConversations: () -> Unit,
+) {
+
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is UserAvailable -> navigateToConversations()
+            is AuthRequired -> navigateToAuth()
+            else -> {}
+        }
+    }
 
 }
--- feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
@@ -3,6 +3,10 @@ package io.github.zohrevand.dialogue.feature.router
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.zohrevand.dialogue.core.data.repository.PreferencesRepository
+import io.github.zohrevand.dialogue.feature.router.RouterUiState.Loading
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +14,15 @@ class RouterViewModel @Inject constructor(
     private val preferencesRepository: PreferencesRepository
 ) : ViewModel(){
 
+    private val _uiState: MutableStateFlow<RouterUiState> = MutableStateFlow(Loading)
+    val uiState: StateFlow<RouterUiState> = _uiState.asStateFlow()
 
+}
+
+sealed interface RouterUiState {
+    object Loading : RouterUiState
+
+    object UserAvailable : RouterUiState
+
+    object AuthRequired : RouterUiState
 }
