db3c671aea9a129a768af33c6f22eb0391936e3e
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 22:18:33 +0430

    Add initializeRoster to get Roster from connection

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManager.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManager.kt
@@ -1,4 +1,8 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+
 interface RosterManager {
+
+    fun initializeRoster(connection: XMPPTCPConnection)
 }
--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
@@ -2,11 +2,22 @@ package io.github.zohrevand.dialogue.core.xmpp
 
 import org.jivesoftware.smack.roster.Roster
 import org.jivesoftware.smack.roster.Roster.SubscriptionMode.accept_all
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import java.lang.IllegalStateException
 import javax.inject.Inject
 
 class RosterManagerImpl @Inject constructor() : RosterManager {
 
+    private var roster: Roster? = null
+
     init {
         Roster.setDefaultSubscriptionMode(accept_all)
+    }
+
+    private fun getRoster(): Roster =
+        roster ?: throw IllegalStateException("You should call initializeRoster() first.")
+
+    override fun initializeRoster(connection: XMPPTCPConnection) {
+        roster = Roster.getInstanceFor(connection)
     }
 }
