4b3d5468c5434416daefa4b32448f2d69493c9fe
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 22:23:15 +0430

    Make roster variable late initialized

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
@@ -11,20 +11,17 @@ import javax.inject.Inject
 
 class RosterManagerImpl @Inject constructor() : RosterManager {
 
-    private var roster: Roster? = null
+    private lateinit var roster: Roster
 
     init {
         Roster.setDefaultSubscriptionMode(accept_all)
         Roster.setRosterLoadedAtLoginDefault(true)
     }
 
-    private fun getRoster(): Roster =
-        roster ?: throw IllegalStateException("You should call initializeRoster() first.")
-
     override fun initializeRoster(connection: XMPPTCPConnection) {
         roster = Roster.getInstanceFor(connection)
 
-        getRoster().addRosterListener(object : RosterListener {
+        roster.addRosterListener(object : RosterListener {
             override fun entriesAdded(addresses: MutableCollection<Jid>?) {
                 TODO("Not yet implemented")
             }
