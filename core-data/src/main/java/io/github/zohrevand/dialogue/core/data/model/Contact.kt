c954e503659ea80cfd6300a782ed03ebe9f41dc1
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 23:58:55 +0430

    Add Contact to ContactEntity mapper

---
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/model/Contact.kt
@@ -0,0 +1,13 @@
+package io.github.zohrevand.dialogue.core.data.model
+
+import io.github.zohrevand.core.model.data.Contact
+import io.github.zohrevand.dialogue.core.database.model.ContactEntity
+
+fun Contact.asEntity() = ContactEntity(
+    jid = jid,
+    presenceType = presence.type,
+    presenceMode = presence.mode,
+    presenceStatus = presence.status,
+    presencePriority = presence.priority,
+    lastTime = lastTime
+)
