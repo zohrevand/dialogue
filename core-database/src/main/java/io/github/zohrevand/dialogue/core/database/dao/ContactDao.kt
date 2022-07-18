899e56424430fc96447293046b37a9c1bd1f4622
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 23:54:26 +0430

    Add getContactEntity to ContactDao

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/ContactDao.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/ContactDao.kt
@@ -1,12 +1,20 @@
 package io.github.zohrevand.dialogue.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import io.github.zohrevand.dialogue.core.database.model.ContactEntity
+import kotlinx.coroutines.flow.Flow
 
 /**
  * DAO for [ContactEntity] access
  */
 @Dao
 interface ContactDao {
-
+    @Query(
+        value = """
+        SELECT * FROM contacts
+        WHERE jid = :jid
+    """
+    )
+    fun getContactEntity(jid: String): Flow<ContactEntity>
 }
