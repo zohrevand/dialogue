a12fee43aeb4b58334f85bf9cffbc15bd8a01fdd
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 22:22:03 +0430

    Add RosterListener to roster

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
@@ -1,8 +1,11 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.packet.Presence
 import org.jivesoftware.smack.roster.Roster
 import org.jivesoftware.smack.roster.Roster.SubscriptionMode.accept_all
+import org.jivesoftware.smack.roster.RosterListener
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import org.jxmpp.jid.Jid
 import java.lang.IllegalStateException
 import javax.inject.Inject
 
@@ -20,5 +23,25 @@ class RosterManagerImpl @Inject constructor() : RosterManager {
 
     override fun initializeRoster(connection: XMPPTCPConnection) {
         roster = Roster.getInstanceFor(connection)
+
+        getRoster().addRosterListener(object : RosterListener {
+            override fun entriesAdded(addresses: MutableCollection<Jid>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun entriesUpdated(addresses: MutableCollection<Jid>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun entriesDeleted(addresses: MutableCollection<Jid>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun presenceChanged(presence: Presence?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
+
+
 }
