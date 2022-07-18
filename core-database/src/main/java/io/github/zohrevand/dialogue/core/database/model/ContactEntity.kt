1377757bb92b825e32a8da6b812abd34bf7d2435
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 23:41:21 +0430

    Add ContactEntity properties

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ContactEntity.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ContactEntity.kt
@@ -1,10 +1,23 @@
 package io.github.zohrevand.dialogue.core.database.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import io.github.zohrevand.core.model.data.Presence
+import kotlinx.datetime.Instant
 
 @Entity
 data class ContactEntity(
     @PrimaryKey
-    val jid: String
+    val jid: String,
+    @ColumnInfo(name = "presence_type")
+    val presenceType: Presence.Type,
+    @ColumnInfo(name = "presence_mode")
+    val presenceMode: Presence.Mode,
+    @ColumnInfo(name = "presence_status")
+    val presenceStatus: String,
+    @ColumnInfo(name = "presence_priority")
+    val presencePriority: Int,
+    @ColumnInfo(name = "last_time")
+    val lastTime: Instant
 )
