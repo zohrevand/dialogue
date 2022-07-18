33494e147dc8fe9c82956dea922a09c81456ac84
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-18 22:13:38 +0430

    Set Roster defaultSubscriptionMode to accept_all

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
@@ -1,7 +1,12 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.roster.Roster
+import org.jivesoftware.smack.roster.Roster.SubscriptionMode.accept_all
 import javax.inject.Inject
 
 class RosterManagerImpl @Inject constructor() : RosterManager {
 
+    init {
+        Roster.setDefaultSubscriptionMode(accept_all)
+    }
 }
