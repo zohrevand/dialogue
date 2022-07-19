5b8471d6bdb7455e28638f96154d82ed54ea2606
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-19 14:38:13 +0430

    Comment out spotless license header check

--- build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -16,19 +16,19 @@ class SpotlessConventionPlugin : Plugin<Project> {
                     target("**/*.kt")
                     targetExclude("**/build/**/*.kt")
                     ktlint(libs.findVersion("ktlint").get().toString()).userData(mapOf("android" to "true"))
-                    licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+//                    licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
                 }
                 format("kts") {
                     target("**/*.kts")
                     targetExclude("**/build/**/*.kts")
                     // Look for the first line that doesn't have a block comment (assumed to be the license)
-                    licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+//                    licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
                 }
                 format("xml") {
                     target("**/*.xml")
                     targetExclude("**/build/**/*.xml")
                     // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
-                    licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+//                    licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
                 }
             }
         }
