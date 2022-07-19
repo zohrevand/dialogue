4f8db88b9abcd6b6f3335f612f4646008e522124
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-19 14:48:07 +0430

    Apply spotless feature-router module

--- feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
+++ feature-router/src/main/java/io/github/zohrevand/dialogue/feature/router/RouterViewModel.kt
@@ -8,18 +8,18 @@ import io.github.zohrevand.dialogue.core.data.repository.PreferencesRepository
 import io.github.zohrevand.dialogue.feature.router.RouterUiState.AuthRequired
 import io.github.zohrevand.dialogue.feature.router.RouterUiState.Loading
 import io.github.zohrevand.dialogue.feature.router.RouterUiState.UserAvailable
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class RouterViewModel @Inject constructor(
     private val preferencesRepository: PreferencesRepository
-) : ViewModel(){
+) : ViewModel() {
 
     private val _uiState: MutableStateFlow<RouterUiState> = MutableStateFlow(Loading)
     val uiState: StateFlow<RouterUiState> = _uiState.asStateFlow()
