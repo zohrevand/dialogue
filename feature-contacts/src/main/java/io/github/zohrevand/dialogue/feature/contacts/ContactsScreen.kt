cf71a40acecdcf98280ffab575488241250a756a
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-20 23:44:02 +0430

    Connect ContactsScreen and ContactsViewModel

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
@@ -3,22 +3,27 @@ package io.github.zohrevand.dialogue.feature.contacts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun ContactsRoute(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: ContactsViewModel = hiltViewModel()
 ) {
     ContactsScreen(
+        addContact = viewModel::addContact,
         modifier = modifier
     )
 }
 
 @Composable
 fun ContactsScreen(
+    addContact: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -27,5 +32,8 @@ fun ContactsScreen(
             .background(Color.Cyan)
     ) {
         Text(text = "This is Contacts Screen")
+        Button(onClick = addContact) {
+            Text(text = "Add Contact")
+        }
     }
 }
--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
@@ -2,6 +2,7 @@ package io.github.zohrevand.dialogue.feature.contacts
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.zohrevand.core.model.data.Contact
 import io.github.zohrevand.core.model.data.Presence
 import io.github.zohrevand.dialogue.core.data.repository.ContactsRepository
@@ -9,6 +10,7 @@ import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import javax.inject.Inject
 
+@HiltViewModel
 class ContactsViewModel @Inject constructor(
     private val contactsRepository: ContactsRepository
 ) : ViewModel() {
