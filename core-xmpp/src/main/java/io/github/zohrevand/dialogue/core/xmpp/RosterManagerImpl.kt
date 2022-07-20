92552488fddd6154b2ab0277f81d24eff6a977cc
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-20 13:00:19 +0430

    Add logs to roster listeners

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/RosterManagerImpl.kt
@@ -1,5 +1,6 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import android.util.Log
 import javax.inject.Inject
 import org.jivesoftware.smack.packet.Presence
 import org.jivesoftware.smack.roster.PresenceEventListener
@@ -11,6 +12,8 @@ import org.jxmpp.jid.BareJid
 import org.jxmpp.jid.FullJid
 import org.jxmpp.jid.Jid
 import org.jxmpp.jid.impl.JidCreate
+
+private const val TAG = "RosterManager"
 
 class RosterManagerImpl @Inject constructor() : RosterManager {
 
@@ -36,19 +39,19 @@ class RosterManagerImpl @Inject constructor() : RosterManager {
     private fun Roster.addRosterListener() {
         rosterListener = object : RosterListener {
             override fun entriesAdded(addresses: MutableCollection<Jid>?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "EntriesAdded $addresses")
             }
 
             override fun entriesUpdated(addresses: MutableCollection<Jid>?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "entriesUpdated $addresses")
             }
 
             override fun entriesDeleted(addresses: MutableCollection<Jid>?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "entriesDeleted $addresses")
             }
 
             override fun presenceChanged(presence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceChanged $presence")
             }
         }
 
@@ -58,23 +61,23 @@ class RosterManagerImpl @Inject constructor() : RosterManager {
     private fun Roster.addPresenceEventListener() {
         presenceEventListener = object : PresenceEventListener {
             override fun presenceAvailable(address: FullJid?, availablePresence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceAvailable $address $availablePresence")
             }
 
             override fun presenceUnavailable(address: FullJid?, presence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceUnavailable $address $presence")
             }
 
             override fun presenceError(address: Jid?, errorPresence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceError $address $errorPresence")
             }
 
             override fun presenceSubscribed(address: BareJid?, subscribedPresence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceSubscribed $address $subscribedPresence")
             }
 
             override fun presenceUnsubscribed(address: BareJid?, unsubscribedPresence: Presence?) {
-                TODO("Not yet implemented")
+                Log.d(TAG, "presenceError $address $unsubscribedPresence")
             }
         }
 
@@ -83,6 +86,7 @@ class RosterManagerImpl @Inject constructor() : RosterManager {
 
     private fun createEntry(jid: String) {
         // TODO: check if the nickname (name) is required
+        // TODO: check if preApproveAndCreateEntry is the right way to create entry
         roster.preApproveAndCreateEntry(JidCreate.bareFrom(jid), null, null)
     }
 
