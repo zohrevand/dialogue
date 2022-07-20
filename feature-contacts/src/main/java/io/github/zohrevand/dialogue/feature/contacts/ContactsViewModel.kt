1afd6c1c8522a3d3204d52e81ac4c31e9209f3f0
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-20 23:23:06 +0430

    Add contactsRepository to ContactsViewModel and Hilt

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
@@ -1,6 +1,11 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
 import androidx.lifecycle.ViewModel
+import io.github.zohrevand.dialogue.core.data.repository.ContactsRepository
+import javax.inject.Inject
 
-class ContactsViewModel : ViewModel() {
+class ContactsViewModel @Inject constructor(
+    private val contactsRepository: ContactsRepository
+) : ViewModel() {
+
 }
