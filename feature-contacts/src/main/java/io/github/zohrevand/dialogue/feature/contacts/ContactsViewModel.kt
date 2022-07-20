d21925cc7351a844064dbc85547ab6bf3cacedd6
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-20 23:28:37 +0430

    Add addContact method to ContactsViewModel

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModel.kt
@@ -1,11 +1,28 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.zohrevand.core.model.data.Contact
+import io.github.zohrevand.core.model.data.Presence
 import io.github.zohrevand.dialogue.core.data.repository.ContactsRepository
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
 import javax.inject.Inject
 
 class ContactsViewModel @Inject constructor(
     private val contactsRepository: ContactsRepository
 ) : ViewModel() {
 
+    fun addContact() {
+        viewModelScope.launch {
+            contactsRepository.updateContacts(listOf(
+                Contact(
+                    jid = "zohrevand@jix.im",
+                    presence = Presence(),
+                    lastTime = Clock.System.now(),
+                    addToRoster = true
+                )
+            ))
+        }
+    }
 }
