7354dd1daa7f6613b14bddf3724a7a63f73a47bc
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-22 22:44:14 +0430

    Create RosterEntry mapper to Contact

---
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/model/Contact.kt
@@ -0,0 +1,13 @@
+package io.github.zohrevand.dialogue.core.xmpp.model
+
+import io.github.zohrevand.core.model.data.Contact
+import io.github.zohrevand.core.model.data.Presence
+import kotlinx.datetime.Clock
+import org.jivesoftware.smack.roster.RosterEntry
+
+fun RosterEntry.asExternalModel() = Contact(
+    jid = jid.asBareJid().toString(),
+    presence = Presence(),
+    lastTime = Clock.System.now(),
+    addToRoster = false
+)
