08ecde2fc781bd36c615fd38be21f57703e87720
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-23 23:59:43 +0430

    Display an avatar for each contact

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
@@ -1,14 +1,18 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
@@ -16,7 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -73,6 +79,17 @@ fun ContactItem(contact: Contact) {
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.height(80.dp)
     ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(56.dp)
+                .clip(CircleShape)
+                .background(Color.Cyan)
+                .padding(end = 16.dp)
+        ) {
+            Text(text = contact.jid.take(1).uppercase())
+        }
+
         Text(text = contact.jid)
     }
 }
