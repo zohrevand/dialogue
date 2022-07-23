eff70efac2fcd158a82dd8788fb9ca8625d68260
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-23 23:56:56 +0430

    Display a Divider after each contact

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -47,6 +50,7 @@ fun ContactsScreen(
         ) {
             items(uiState.contacts) {
                 ContactItem(it)
+                Divider(color = Color(0xFFDFDFDF))
             }
         }
     }
@@ -65,7 +69,10 @@ fun ContactsScreen(
 
 @Composable
 fun ContactItem(contact: Contact) {
-    Row(modifier = Modifier.height(80.dp)) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.height(80.dp)
+    ) {
         Text(text = contact.jid)
     }
 }
