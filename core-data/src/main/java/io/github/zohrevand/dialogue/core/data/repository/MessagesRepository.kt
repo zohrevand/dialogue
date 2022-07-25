862088ef46694934ee4642bdf2fdc401f16e8d8f
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-25 23:37:46 +0430

    Add getMessage to repository

--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/MessagesRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/MessagesRepository.kt
@@ -1,5 +1,9 @@
 package io.github.zohrevand.dialogue.core.data.repository
 
+import io.github.zohrevand.core.model.data.Message
+import kotlinx.coroutines.flow.Flow
+
 interface MessagesRepository {
 
+    fun getMessage(id: String): Flow<Message>
 }
--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstMessagesRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstMessagesRepository.kt
@@ -1,10 +1,16 @@
 package io.github.zohrevand.dialogue.core.data.repository
 
+import io.github.zohrevand.core.model.data.Message
 import io.github.zohrevand.dialogue.core.database.dao.MessageDao
+import io.github.zohrevand.dialogue.core.database.model.asExternalModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class OfflineFirstMessagesRepository @Inject constructor(
     private val messageDao: MessageDao
 ) : MessagesRepository {
 
+    override fun getMessage(id: String): Flow<Message> =
+        messageDao.getMessageEntity(id).map { it.asExternalModel() }
 }
