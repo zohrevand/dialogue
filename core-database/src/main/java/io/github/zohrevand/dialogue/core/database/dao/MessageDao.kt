e6a576280a0760356e4f2dd6bc076e42dbb74dcc
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-25 23:31:32 +0430

    MessageDao getMessageEntitiesStream by ids

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/MessageDao.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/MessageDao.kt
@@ -1,6 +1,8 @@
 package io.github.zohrevand.dialogue.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.github.zohrevand.dialogue.core.database.model.MessageEntity
 import kotlinx.coroutines.flow.Flow
@@ -20,4 +22,12 @@ interface MessageDao {
 
     @Query(value = "SELECT * FROM messages")
     fun getMessageEntitiesStream(): Flow<List<MessageEntity>>
+
+    @Query(
+        value = """
+        SELECT * FROM messages
+        WHERE id IN (:ids)
+    """
+    )
+    fun getMessageEntitiesStream(ids: Set<String>): Flow<List<MessageEntity>>
 }
