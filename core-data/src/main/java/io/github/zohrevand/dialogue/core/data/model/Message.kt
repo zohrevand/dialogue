f084b5a370bd43358171837127f7e7990a1849b7
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-25 23:21:46 +0430

    Mapper from Message to MessageEntity

---
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/model/Message.kt
@@ -0,0 +1,12 @@
+package io.github.zohrevand.dialogue.core.data.model
+
+import io.github.zohrevand.core.model.data.Message
+import io.github.zohrevand.dialogue.core.database.model.MessageEntity
+
+fun Message.asEntity() = MessageEntity(
+    id = id,
+    fromJid = fromJid,
+    toJid = toJid,
+    body = body,
+    time = time
+)
