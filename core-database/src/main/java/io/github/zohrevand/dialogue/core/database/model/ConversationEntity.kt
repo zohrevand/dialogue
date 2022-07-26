b6b2756ea1c638f888c576fe4c83a1a3a8b80fd2
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 19:14:56 +0430

    Mapper from ConversationEntity to Conversation

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ConversationEntity.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ConversationEntity.kt
@@ -3,10 +3,15 @@ package io.github.zohrevand.dialogue.core.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import io.github.zohrevand.core.model.data.Conversation
 
 @Entity(tableName = "conversations")
 data class ConversationEntity(
     @PrimaryKey
     @ColumnInfo(name = "with_jid")
     val withJid: String
+)
+
+fun ConversationEntity.asExternalModel() = Conversation(
+    withJid = withJid
 )
