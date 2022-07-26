b0e418ebbb5451d6c0a059cc81963a71e5618c7f
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 15:54:20 +0430

    ConversationDao getConversationEntity by withJid

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/ConversationDao.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/ConversationDao.kt
@@ -1,12 +1,20 @@
 package io.github.zohrevand.dialogue.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
+import kotlinx.coroutines.flow.Flow
 
 /**
  * DAO for [ConversationEntity] access
  */
 @Dao
 interface ConversationDao {
-
+    @Query(
+        value = """
+        SELECT * FROM conversations
+        WHERE with_jid = :withJid
+    """
+    )
+    fun getConversationEntity(withJid: String): Flow<ConversationEntity>
 }
