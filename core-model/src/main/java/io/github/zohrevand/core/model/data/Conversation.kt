1842e8610172e681f885feb59d9fe4462c1a1d0a
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 19:47:02 +0430

    Add lastMessage and unreadMessagesCount to Conversation

--- core-model/src/main/java/io/github/zohrevand/core/model/data/Conversation.kt
+++ core-model/src/main/java/io/github/zohrevand/core/model/data/Conversation.kt
@@ -2,5 +2,7 @@ package io.github.zohrevand.core.model.data
 
 data class Conversation(
     val peerJid: String,
-    val draftMessage: String? = null
+    val draftMessage: String? = null,
+    val lastMessage: Message? = null,
+    val unreadMessagesCount: Int = 0
 )
