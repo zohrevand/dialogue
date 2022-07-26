040fcdfc79c9c101113c9a22447d81607770b22b
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 14:56:36 +0430

    Add ConversationEntity to database entities

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/DialogueDatabase.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/DialogueDatabase.kt
@@ -6,13 +6,15 @@ import androidx.room.TypeConverters
 import io.github.zohrevand.dialogue.core.database.dao.ContactDao
 import io.github.zohrevand.dialogue.core.database.dao.MessageDao
 import io.github.zohrevand.dialogue.core.database.model.ContactEntity
+import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
 import io.github.zohrevand.dialogue.core.database.model.MessageEntity
 import io.github.zohrevand.dialogue.core.database.util.InstantConverter
 
 @Database(
     entities = [
         ContactEntity::class,
-        MessageEntity::class
+        MessageEntity::class,
+        ConversationEntity::class
     ],
     version = 1,
     exportSchema = true
