1f88ea719000f821d97c7841f95a07718c669ee9
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 19:13:55 +0430

    Mapper from Conversation to ConversationEntity

---
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/model/Conversation.kt
@@ -0,0 +1,8 @@
+package io.github.zohrevand.dialogue.core.data.model
+
+import io.github.zohrevand.core.model.data.Conversation
+import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
+
+fun Conversation.asEntity() = ConversationEntity(
+    withJid = withJid
+)
