7fda65d418cf7278c44eefba855542c930da3f3d
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-26 19:15:44 +0430

    ConversationRepository getConversation by withJid

--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/ConversationsRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/ConversationsRepository.kt
@@ -1,4 +1,9 @@
 package io.github.zohrevand.dialogue.core.data.repository
 
+import io.github.zohrevand.core.model.data.Conversation
+import kotlinx.coroutines.flow.Flow
+
 interface ConversationsRepository {
+
+    fun getConversation(withJid: String): Flow<Conversation>
 }
--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstConversationsRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstConversationsRepository.kt
@@ -1,10 +1,17 @@
 package io.github.zohrevand.dialogue.core.data.repository
 
+import io.github.zohrevand.core.model.data.Conversation
 import io.github.zohrevand.dialogue.core.database.dao.ConversationDao
+import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
+import io.github.zohrevand.dialogue.core.database.model.asExternalModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class OfflineFirstConversationsRepository @Inject constructor(
     private val conversationDao: ConversationDao
 ) : ConversationsRepository {
 
+    override fun getConversation(withJid: String): Flow<Conversation> =
+        conversationDao.getConversationEntity(withJid).map(ConversationEntity::asExternalModel)
 }
