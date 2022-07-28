2579487364ef54fd6ca5134de87bc7c74a3e33f7
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-28 23:44:13 +0430

    Collect chat uiState and pass it to ChatScreen

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
@@ -6,15 +6,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Success
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun ChatRoute(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: ChatViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     ChatScreen(
+        uiState = uiState,
         onBackClick = onBackClick,
         modifier = modifier
     )
@@ -22,17 +32,20 @@ fun ChatRoute(
 
 @Composable
 fun ChatScreen(
+    uiState: ChatUiState,
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Yellow)
-    ) {
-        Text(text = "This is Chat Screen")
-        Button(onClick = onBackClick) {
-            Text(text = "Back to conversations")
+    if (uiState is Success) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+        ) {
+            Text(text = "This is Chat Screen for ${uiState.chatId}")
+            Button(onClick = onBackClick) {
+                Text(text = "Back to conversations")
+            }
         }
     }
 }
