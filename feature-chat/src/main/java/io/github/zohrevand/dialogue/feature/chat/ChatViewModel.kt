5f0b0103e6dde7cd9b7b91a953dd29fde6f379b9
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-28 23:32:02 +0430

    Create ChatViewModel

---
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
@@ -0,0 +1,12 @@
+package io.github.zohrevand.dialogue.feature.chat
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
