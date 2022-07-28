2450ab8e9b23dc657e4096c5ac44e7c00e926ddf
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-28 22:59:50 +0430

    Add ChatDestination createNavigationRoute

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/navigation/ChatNavigation.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/navigation/ChatNavigation.kt
@@ -1,13 +1,23 @@
 package io.github.zohrevand.dialogue.feature.chat.navigation
 
+import android.net.Uri
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import io.github.zohrevand.dialogue.core.navigation.DialogueNavigationDestination
 import io.github.zohrevand.dialogue.feature.chat.ChatRoute
 
 object ChatDestination : DialogueNavigationDestination {
-    override val route = "chat_route"
+    private const val contactJidArg = "contactJid"
+    override val route = "chat_route/{$contactJidArg}"
     override val destination = "chat_destination"
+
+    /**
+     * Creates destination route for a contactJid that could include special characters
+     */
+    fun createNavigationRoute(contactJidArg: String): String {
+        val encodedJid = Uri.encode(contactJidArg)
+        return "topic_route/$encodedJid"
+    }
 }
 
 fun NavGraphBuilder.chatGraph(
