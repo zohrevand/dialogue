9d596adc6fab59bab161b8d828a444f09dae15a8
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-28 23:35:42 +0430

    Add contactId from savedStateHandle

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
@@ -1,12 +1,17 @@
 package io.github.zohrevand.dialogue.feature.chat
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.zohrevand.dialogue.feature.chat.navigation.ChatDestination
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
+    private val contactId: String = checkNotNull(
+        savedStateHandle[ChatDestination.contactJidArg]
+    )
 }
