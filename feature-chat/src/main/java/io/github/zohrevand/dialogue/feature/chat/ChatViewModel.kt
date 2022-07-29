bf8d51acefd390d9fcec259afaa7600e576867b2
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-29 21:07:41 +0430

    Collect conversation and start it if it is not started

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
@@ -2,30 +2,45 @@ package io.github.zohrevand.dialogue.feature.chat
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.zohrevand.core.model.data.ConversationStatus.NotStarted
+import io.github.zohrevand.core.model.data.ConversationStatus.Started
+import io.github.zohrevand.dialogue.core.data.repository.ConversationsRepository
+import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Loading
 import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Success
 import io.github.zohrevand.dialogue.feature.chat.navigation.ChatDestination
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    conversationsRepository: ConversationsRepository
 ) : ViewModel() {
 
     private val contactId: String = checkNotNull(
         savedStateHandle[ChatDestination.contactJidArg]
     )
 
-    private val _uiState: MutableStateFlow<ChatUiState> = MutableStateFlow(ChatUiState.Loading)
-    val uiState: StateFlow<ChatUiState> = _uiState.asStateFlow()
-
-    init {
-        _uiState.update { Success(contactId) }
-    }
+    val uiState: StateFlow<ChatUiState> =
+        conversationsRepository.getConversation(peerJid = contactId)
+            .map { conversation ->
+                if (conversation.status == NotStarted) {
+                    conversationsRepository.updateConversation(conversation.copy(status = Started))
+                    Loading
+                } else {
+                    Success(conversation.peerJid)
+                }
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = Loading
+            )
 }
 
 sealed interface ChatUiState {
