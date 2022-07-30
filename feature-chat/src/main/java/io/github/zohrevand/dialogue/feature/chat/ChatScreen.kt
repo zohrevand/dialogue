513bde36b0e0fd69cb6b0396507963c69bfa1bf3
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-30 22:36:03 +0430

    Change OutlineTextField to TextField

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
@@ -11,8 +11,8 @@ import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -73,7 +73,7 @@ fun ChatScreen(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            OutlinedTextField(
+            TextField(
                 value = messageText,
                 onValueChange = setMessageText,
                 label = { Text(text = stringResource(message_label)) },
