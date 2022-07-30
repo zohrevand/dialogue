7d6ebd492a9c0f2482132d3a3e3fbb98969d9bc0
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-30 23:09:09 +0430

    Rename DialogueApp to DialogueApplication.kt

--- app/src/main/java/io/github/zohrevand/dialogue/DialogueApplication.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/DialogueApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import org.jivesoftware.smack.android.AndroidSmackInitializer
 
 @HiltAndroidApp
-class DialogueApp : Application() {
+class DialogueApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
