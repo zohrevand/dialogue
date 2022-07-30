42556ee583d5d7981bc1309ee8b1863cb572e8a7
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-30 22:24:57 +0430

    Message text field and send button

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatScreen.kt
@@ -2,17 +2,32 @@ package io.github.zohrevand.dialogue.feature.chat
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Success
+import io.github.zohrevand.dialogue.feature.chat.R.string.message_label
+import io.github.zohrevand.dialogue.feature.chat.R.string.send
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
@@ -36,15 +51,41 @@ fun ChatScreen(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    if (uiState is Success) {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .background(Color.Yellow)
+    val (messageText, setMessageText) = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (uiState is Success) {
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(Color.Yellow)
+            ) {
+                Text(text = "This is Chat Screen for ${uiState.conversation.peerJid}")
+                Button(onClick = onBackClick) {
+                    Text(text = "Back")
+                }
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "This is Chat Screen for ${uiState.conversation.peerJid}")
-            Button(onClick = onBackClick) {
-                Text(text = "Back")
+            OutlinedTextField(
+                value = messageText,
+                onValueChange = setMessageText,
+                label = { Text(text = stringResource(message_label)) },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                modifier = modifier.fillMaxWidth().weight(1f)
+            )
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Filled.Send,
+                    contentDescription = stringResource(send),
+                    tint = Color.Blue
+                )
             }
         }
     }
