3dadb3ddd467f15bbab62dcd67aca7767abd77bd
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-31 00:21:09 +0430

    Remove setting status bar color

--- app/src/main/java/io/github/zohrevand/dialogue/ui/theme/Theme.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/theme/Theme.kt
@@ -62,15 +62,6 @@ fun DialogueTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
