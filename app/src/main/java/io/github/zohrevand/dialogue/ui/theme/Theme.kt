e4149ce18f171800effbfad781c572fb60d0583b
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-31 00:21:37 +0430

    Remove unused imports

--- app/src/main/java/io/github/zohrevand/dialogue/ui/theme/Theme.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package io.github.zohrevand.dialogue.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
@@ -10,13 +9,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
