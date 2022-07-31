1bb00bbb3e562409367dfa71cb55528f39b26baa
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-31 18:13:14 +0430

    Initialize ChatManager

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManager.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManager.kt
@@ -1,5 +1,8 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+
 interface MessageManager {
 
+    suspend fun initialize(connection: XMPPTCPConnection)
 }
--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
@@ -1,9 +1,16 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
+import org.jivesoftware.smack.chat2.ChatManager
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import javax.inject.Inject
 
 class MessageManagerImpl @Inject constructor(
 
 ) : MessageManager {
 
+    private lateinit var chatManager: ChatManager
+
+    override suspend fun initialize(connection: XMPPTCPConnection) {
+        chatManager = ChatManager.getInstanceFor(connection)
+    }
 }
