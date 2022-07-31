87bd4c9cb9f3308d6ace7ac6878f3062f6f7ca65
Hasan Zohrevand <zohrevan@gmail.com>
2022-07-31 19:40:20 +0430

    Add Coroutine scope to MessageManagerImpl

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
@@ -1,6 +1,9 @@
 package io.github.zohrevand.dialogue.core.xmpp
 
 import io.github.zohrevand.dialogue.core.xmpp.collector.MessagesCollector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import org.jivesoftware.smack.chat2.ChatManager
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import javax.inject.Inject
@@ -9,6 +12,8 @@ class MessageManagerImpl @Inject constructor(
     private val messagesCollector: MessagesCollector
 ) : MessageManager {
 
+    private val scope = CoroutineScope(SupervisorJob())
+
     private lateinit var chatManager: ChatManager
 
     override suspend fun initialize(connection: XMPPTCPConnection) {
@@ -16,6 +21,6 @@ class MessageManagerImpl @Inject constructor(
     }
 
     override fun onCleared() {
-
+        scope.cancel()
     }
 }
