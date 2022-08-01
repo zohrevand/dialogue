82c56e6428bdb1509de0ed8f2406018ac67c3f8e
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-01 17:03:24 +0430

    Refactor MessageManagerImpl

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/MessageManagerImpl.kt
@@ -12,15 +12,19 @@ import org.jivesoftware.smack.chat2.ChatManager
 import org.jivesoftware.smack.chat2.IncomingChatMessageListener
 import org.jivesoftware.smack.chat2.OutgoingChatMessageListener
 import org.jivesoftware.smack.packet.MessageBuilder
+import org.jivesoftware.smack.packet.Stanza
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import org.jivesoftware.smackx.chatstates.ChatState
 import org.jivesoftware.smackx.chatstates.ChatStateListener
 import org.jivesoftware.smackx.chatstates.ChatStateManager
 import org.jivesoftware.smackx.receipts.DeliveryReceiptManager
 import org.jivesoftware.smackx.receipts.DeliveryReceiptManager.AutoReceiptMode.always
 import org.jivesoftware.smackx.receipts.ReceiptReceivedListener
 import org.jxmpp.jid.EntityBareJid
+import org.jxmpp.jid.Jid
 import org.jxmpp.jid.impl.JidCreate
 import javax.inject.Inject
+import org.jivesoftware.smack.packet.Message as SmackMessage
 
 private const val TAG = "MessagesManagerImpl"
 
@@ -49,44 +53,9 @@ class MessageManagerImpl @Inject constructor(
         }
 
         observeIncomingMessages()
-
         observeOutgoingMessages()
-
         observeChatState()
-
         observeDeliveryReceipt()
-    }
-
-    private fun observeIncomingMessages() {
-        incomingChatMessageListener = IncomingChatMessageListener { from, message, chat ->
-            Log.d(TAG, "IncomingListener - from: $from, message: $message, chat: $chat")
-        }
-        chatManager.addIncomingListener(incomingChatMessageListener)
-    }
-
-    private fun observeOutgoingMessages() {
-        outgoingChatMessageListener = OutgoingChatMessageListener { to, messageBuilder, chat ->
-            Log.d(TAG, "OutgoingListener - to: $to, messageBuilder: $messageBuilder, chat: $chat")
-        }
-        chatManager.addOutgoingListener(outgoingChatMessageListener)
-    }
-
-    private fun observeChatState() {
-        chatStateListener = ChatStateListener { chat, state, message ->
-            Log.d(TAG, "ChatStateListener - state: $state, message: $message, chat: $chat")
-        }
-        chatStateManager.addChatStateListener(chatStateListener)
-    }
-
-    private fun observeDeliveryReceipt() {
-        deliveryReceiptManager.autoReceiptMode = always
-        receiptReceivedListener = ReceiptReceivedListener { fromJid, toJid, receiptId, receipt ->
-            Log.d(
-                TAG,
-                "addReceiptReceivedListener - fromJid: $fromJid, toJid: $toJid, receiptId: $receiptId, receipt: $receipt"
-            )
-        }
-        deliveryReceiptManager.addReceiptReceivedListener(receiptReceivedListener)
     }
 
     // blocking
@@ -100,6 +69,63 @@ class MessageManagerImpl @Inject constructor(
 
             chat.send(smackMessage)
         }
+    }
+
+    private fun observeIncomingMessages() {
+        incomingChatMessageListener = IncomingChatMessageListener(::handleIncomingMessage)
+        chatManager.addIncomingListener(incomingChatMessageListener)
+    }
+
+    private fun observeOutgoingMessages() {
+        outgoingChatMessageListener = OutgoingChatMessageListener(::handleOutgoingMessage)
+        chatManager.addOutgoingListener(outgoingChatMessageListener)
+    }
+
+    private fun observeChatState() {
+        chatStateListener = ChatStateListener(::handleChatState)
+        chatStateManager.addChatStateListener(chatStateListener)
+    }
+
+    private fun observeDeliveryReceipt() {
+        deliveryReceiptManager.autoReceiptMode = always
+        receiptReceivedListener = ReceiptReceivedListener(::handleReceivedReceipt)
+        deliveryReceiptManager.addReceiptReceivedListener(receiptReceivedListener)
+    }
+
+    private fun handleIncomingMessage(
+        from: EntityBareJid,
+        message: SmackMessage,
+        chat: Chat?
+    ) {
+        Log.d(TAG, "IncomingListener - from: $from, message: $message, chat: $chat")
+    }
+
+    private fun handleOutgoingMessage(
+        to: EntityBareJid,
+        messageBuilder: MessageBuilder,
+        chat: Chat?
+    ) {
+        Log.d(TAG, "OutgoingListener - to: $to, messageBuilder: $messageBuilder, chat: $chat")
+    }
+
+    private fun handleChatState(
+        chat: Chat?,
+        state: ChatState,
+        message: SmackMessage
+    ) {
+        Log.d(TAG, "ChatStateListener - state: $state, message: $message, chat: $chat")
+    }
+
+    private fun handleReceivedReceipt(
+        fromJid: Jid?,
+        toJid: Jid?,
+        receiptId: String?,
+        receipt: Stanza?
+    ) {
+        Log.d(
+            TAG,
+            "addReceiptReceivedListener - fromJid: $fromJid, toJid: $toJid, receiptId: $receiptId, receipt: $receipt"
+        )
     }
 
     override fun onCleared() {
