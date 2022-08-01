b68e3b35ffa77f67056ff2fa68e069a1e3d040be
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-01 18:18:08 +0430

    Set chatState to composing when user is typing and to paused when user stop typing

--- feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/ChatViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.zohrevand.core.model.data.ChatState
+import io.github.zohrevand.core.model.data.ChatState.Composing
+import io.github.zohrevand.core.model.data.ChatState.Paused
 import io.github.zohrevand.core.model.data.Conversation
 import io.github.zohrevand.core.model.data.ConversationStatus.NotStarted
 import io.github.zohrevand.core.model.data.ConversationStatus.Started
@@ -13,17 +16,20 @@ import io.github.zohrevand.dialogue.core.data.repository.MessagesRepository
 import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Loading
 import io.github.zohrevand.dialogue.feature.chat.ChatUiState.Success
 import io.github.zohrevand.dialogue.feature.chat.navigation.ChatDestination
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.lang.System.currentTimeMillis
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    conversationsRepository: ConversationsRepository,
+    private val conversationsRepository: ConversationsRepository,
     private val messagesRepository: MessagesRepository
 ) : ViewModel() {
 
@@ -34,6 +40,8 @@ class ChatViewModel @Inject constructor(
     private val conversation = conversationsRepository.getConversation(peerJid = contactId)
 
     private val messages = messagesRepository.getMessagesStream(peerJid = contactId)
+
+    private var inputState = InputState()
 
     val uiState: StateFlow<ChatUiState> =
         combine(
@@ -61,9 +69,39 @@ class ChatViewModel @Inject constructor(
         }
     }
 
+    // TODO: refactor to better idiomatic solution
     fun userTyping() {
+        if (inputState.lastTimeTyped == null) {
+            viewModelScope.launch {
+                // 3 second delay to check if user is still typing
+                delay(3_000)
+                if (inputState.userStoppedTyping) {
+                    inputState = inputState.copy(chatState = Paused, lastTimeTyped = null)
+                    conversationsRepository.updateConversation(
+                        conversation.first().copy(chatState = Paused)
+                    )
+                }
+            }
+        }
 
+        inputState = inputState.copy(chatState = Composing, lastTimeTyped = currentTimeMillis())
+
+        if (inputState.chatState != Composing) {
+            viewModelScope.launch {
+                conversationsRepository.updateConversation(
+                    conversation.first().copy(chatState = Composing)
+                )
+            }
+        }
     }
+}
+
+data class InputState(
+    val chatState: ChatState = ChatState.Active,
+    val lastTimeTyped: Long? = null
+) {
+    val userStoppedTyping: Boolean
+        get() = lastTimeTyped != null && currentTimeMillis() - lastTimeTyped > 3_000
 }
 
 sealed interface ChatUiState {
