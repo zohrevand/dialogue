b0198a341c788e0e219e7302ffd286a6c8b1fa47
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-01 23:46:54 +0430

    Add core-designsystem dependencies

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     id("dialogue.android.library.compose")
     id("dialogue.spotless")
 }
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    debugApi(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.ui.util)
+    api(libs.androidx.compose.runtime)
+
+    // TODO : Remove these dependency once we upgrade to Android Studio Dolphin b/228889042
+    // These dependencies are currently necessary to render Compose previews
+    debugImplementation(libs.androidx.customview.poolingcontainer)
+    debugImplementation(libs.androidx.lifecycle.viewModelCompose)
+    debugImplementation(libs.androidx.savedstate.ktx)
+}
