9450e9f6863c09308ea03ce44ef69a760c548d49
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-02 23:49:08 +0430

    Mapper from EntityBareJid to Conversation

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/model/Conversation.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/model/Conversation.kt
@@ -2,9 +2,16 @@ package io.github.zohrevand.dialogue.core.xmpp.model
 
 import io.github.zohrevand.core.model.data.Conversation
 import io.github.zohrevand.core.model.data.ConversationStatus.NotStarted
+import io.github.zohrevand.core.model.data.ConversationStatus.Started
 import org.jivesoftware.smack.roster.RosterEntry
+import org.jxmpp.jid.EntityBareJid
 
 fun RosterEntry.asConversation() = Conversation(
     peerJid = jid.asBareJid().toString(),
     status = NotStarted
+)
+
+fun EntityBareJid.asConversation() = Conversation(
+    peerJid = asBareJid().toString(),
+    status = Started
 )
