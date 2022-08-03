dd7ce4d83fbc194552c3ed739bc529a326da2b87
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-03 13:54:34 +0430

    Update compileSdk and targetSdk to 33

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -17,7 +17,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 32
+                defaultConfig.targetSdk = 33
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
