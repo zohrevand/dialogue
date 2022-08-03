54205f464860da27c015ba6b9eec43d4f24a2aa0
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-03 17:11:23 +0430

    SendingChatStateDao: upsert entity

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/SendingChatStateDao.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/dao/SendingChatStateDao.kt
@@ -1,6 +1,8 @@
 package io.github.zohrevand.dialogue.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
 import io.github.zohrevand.dialogue.core.database.model.SendingChatStateEntity
@@ -14,4 +16,10 @@ interface SendingChatStateDao {
 
     @Query(value = "SELECT * FROM sending_chat_state where consumed = 0")
     fun getSendingChatStateEntitiesStream(): Flow<List<ConversationEntity>>
+
+    /**
+     * Inserts [sendingChatStateEntity] into the db if it doesn't exist, and update if it do
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(sendingChatStateEntity: SendingChatStateEntity)
 }
