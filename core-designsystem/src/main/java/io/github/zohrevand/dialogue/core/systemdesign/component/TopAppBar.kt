211629e5aaa267bb441c98f92ee6dc92d012b4b5
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-03 13:58:26 +0430

    Add ExperimentalMaterial3Api for CenterAlignedTopAppBar

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
@@ -2,6 +2,7 @@ package io.github.zohrevand.dialogue.core.systemdesign.component
 
 import androidx.annotation.StringRes
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DialogueTopAppBar(
     @StringRes titleRes: Int,
