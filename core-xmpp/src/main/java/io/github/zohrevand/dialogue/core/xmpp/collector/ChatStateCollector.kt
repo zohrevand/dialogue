31217ab175dc1d1b1377e1f557724fc6bb70d5fe
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-03 17:26:28 +0430

    Collect SendingChatStates

--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/ChatStateCollector.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/ChatStateCollector.kt
@@ -1,4 +1,9 @@
 package io.github.zohrevand.dialogue.core.xmpp.collector
 
+import io.github.zohrevand.core.model.data.ChatState
+import io.github.zohrevand.core.model.data.SendingChatState
+
 interface ChatStateCollector {
+
+    suspend fun collectChatState(onChatStateChanged: suspend (SendingChatState) -> Unit)
 }
--- core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/ChatStateCollectorImpl.kt
+++ core-xmpp/src/main/java/io/github/zohrevand/dialogue/core/xmpp/collector/ChatStateCollectorImpl.kt
@@ -1,9 +1,19 @@
 package io.github.zohrevand.dialogue.core.xmpp.collector
 
+import io.github.zohrevand.core.model.data.SendingChatState
+import io.github.zohrevand.dialogue.core.data.repository.SendingChatStatesRepository
 import javax.inject.Inject
 
 class ChatStateCollectorImpl @Inject constructor(
-
+    private val sendingChatStatesRepository: SendingChatStatesRepository
 ) : ChatStateCollector {
 
+    override suspend fun collectChatState(onChatStateChanged: suspend (SendingChatState) -> Unit) {
+        sendingChatStatesRepository.getSendingChatStatesStream().collect { states ->
+            states.forEach { sendingChatState ->
+                sendingChatStatesRepository.updateSendingChatState(sendingChatState.copy(consumed = true))
+                onChatStateChanged(sendingChatState)
+            }
+        }
+    }
 }
