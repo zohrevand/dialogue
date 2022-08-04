d5c2d9e424912bede4eedf279ad4eb962133bb3d
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-04 23:22:36 +0430

    Add last message foreign key to ConversationEntity

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ConversationEntity.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/model/ConversationEntity.kt
@@ -2,12 +2,23 @@ package io.github.zohrevand.dialogue.core.database.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import io.github.zohrevand.core.model.data.ChatState
 import io.github.zohrevand.core.model.data.Conversation
 import io.github.zohrevand.core.model.data.ConversationStatus
 
-@Entity(tableName = "conversations")
+@Entity(
+    tableName = "conversations",
+    foreignKeys = [
+        ForeignKey(
+            entity = MessageEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["last_message_id"],
+            onDelete = ForeignKey.NO_ACTION
+        ),
+    ]
+)
 data class ConversationEntity(
     @PrimaryKey
     @ColumnInfo(name = "peer_jid")
