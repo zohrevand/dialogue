9743fd31c7b6bafaa755fa81a90b138933b66a44
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-08 23:37:52 +0430

    Customize top appbar

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
@@ -1,34 +1,31 @@
 package io.github.zohrevand.dialogue.core.systemdesign.component
 
-import androidx.annotation.StringRes
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DialogueTopAppBar(
-    @StringRes titleRes: Int,
+    title: @Composable () -> Unit,
     navigationIcon: ImageVector,
     navigationIconContentDescription: String?,
-    actionIcon: ImageVector,
-    actionIconContentDescription: String?,
     modifier: Modifier = Modifier,
+    actionIcon: ImageVector? = null,
+    actionIconContentDescription: String? = null,
     colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
     onNavigationClick: () -> Unit = {},
     onActionClick: () -> Unit = {}
 ) {
     CenterAlignedTopAppBar(
-        title = { Text(text = stringResource(id = titleRes)) },
+        title = title,
         navigationIcon = {
             IconButton(onClick = onNavigationClick) {
                 Icon(
@@ -39,12 +36,14 @@ fun DialogueTopAppBar(
             }
         },
         actions = {
-            IconButton(onClick = onActionClick) {
-                Icon(
-                    imageVector = actionIcon,
-                    contentDescription = actionIconContentDescription,
-                    tint = MaterialTheme.colorScheme.onSurface
-                )
+            actionIcon?.let {
+                IconButton(onClick = onActionClick) {
+                    Icon(
+                        imageVector = actionIcon,
+                        contentDescription = actionIconContentDescription,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
             }
         },
         colors = colors,
