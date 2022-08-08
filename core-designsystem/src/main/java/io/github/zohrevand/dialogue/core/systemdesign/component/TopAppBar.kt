8afc4406073dacad76c8bfff4e338ff3f6b92289
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-08 23:47:54 +0430

    Overload of DialogueTopAppBar with titleRes parameter

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
@@ -1,15 +1,43 @@
 package io.github.zohrevand.dialogue.core.systemdesign.component
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun DialogueTopAppBar(
+    modifier: Modifier = Modifier,
+    @StringRes titleRes: Int,
+    navigationIcon: ImageVector? = null,
+    navigationIconContentDescription: String? = null,
+    actionIcon: ImageVector? = null,
+    actionIconContentDescription: String? = null,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+    onNavigationClick: () -> Unit = {},
+    onActionClick: () -> Unit = {}
+) {
+    DialogueTopAppBar(
+        modifier = modifier,
+        title = { Text(text = stringResource(id = titleRes)) },
+        navigationIcon = navigationIcon,
+        navigationIconContentDescription = navigationIconContentDescription,
+        actionIcon = actionIcon,
+        actionIconContentDescription = actionIconContentDescription,
+        colors = colors,
+        onNavigationClick = onNavigationClick,
+        onActionClick = onActionClick
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
