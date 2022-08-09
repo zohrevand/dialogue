88e22d17df235501b06ef9e57f189f7e641cecf3
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-09 23:26:10 +0430

    Add MainDispatcherRule to ContactsViewModelTest

--- feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
+++ feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
@@ -1,5 +1,11 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
+import io.github.zohrevand.dialogue.core.testing.util.MainDispatcherRule
+import org.junit.Rule
+
 class ContactsViewModelTest {
+
+    @get:Rule
+    val dispatcherRule = MainDispatcherRule()
 
 }
