4275d2135074add63fa3a91465c954576ee964f4
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-09 23:41:40 +0430

    Add first ContactsViewModelTest test

--- feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
+++ feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
@@ -2,8 +2,11 @@ package io.github.zohrevand.dialogue.feature.contacts
 
 import io.github.zohrevand.dialogue.core.testing.repository.TestContactsRepository
 import io.github.zohrevand.dialogue.core.testing.util.MainDispatcherRule
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class ContactsViewModelTest {
 
@@ -18,5 +21,12 @@ class ContactsViewModelTest {
         viewModel = ContactsViewModel(
             contactsRepository = contactsRepository
         )
+    }
+
+    @Test
+    fun test() = runTest {
+
+        val item = viewModel.uiState.value
+        Assert.assertTrue(item is ContactsUiState.Success)
     }
 }
