fbd0ad51beef503cca0893bb5215efe2e98858c4
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-09 23:28:41 +0430

    Initialize viewModel in setup method

--- feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
+++ feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
@@ -1,6 +1,8 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
+import io.github.zohrevand.dialogue.core.testing.repository.TestContactsRepository
 import io.github.zohrevand.dialogue.core.testing.util.MainDispatcherRule
+import org.junit.Before
 import org.junit.Rule
 
 class ContactsViewModelTest {
@@ -8,4 +10,13 @@ class ContactsViewModelTest {
     @get:Rule
     val dispatcherRule = MainDispatcherRule()
 
+    private val contactsRepository = TestContactsRepository()
+    private lateinit var viewModel: ContactsViewModel
+
+    @Before
+    fun setup() {
+        viewModel = ContactsViewModel(
+            contactsRepository = contactsRepository
+        )
+    }
 }
