ebc2481b64aca7a4958d9f6b854dc496facd0712
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-09 23:11:49 +0430

    Add core-testing module

---
+++ core-testing/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("dialogue.android.library")
+    kotlin("kapt")
+    id("dialogue.spotless")
+}
+
+dependencies {
+    implementation(project(":core-common"))
+    implementation(project(":core-data"))
+    implementation(project(":core-model"))
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    api(libs.junit4)
+    api(libs.androidx.test.core)
+    api(libs.kotlinx.coroutines.test)
+    api(libs.turbine)
+
+    api(libs.androidx.test.espresso.core)
+    api(libs.androidx.test.runner)
+    api(libs.androidx.test.rules)
+    api(libs.androidx.compose.ui.test)
+    api(libs.hilt.android.testing)
+
+    debugApi(libs.androidx.compose.ui.testManifest)
+}
