28fa4ed88d89ec7399d872ee96ecb2f87f0dc1a0
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 15:49:39 +0430

    Add composeTestRule to ContactsScreenTest

--- feature-contacts/src/androidTest/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreenTest.kt
+++ feature-contacts/src/androidTest/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreenTest.kt
@@ -1,4 +1,11 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+
 class ContactsScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 }
