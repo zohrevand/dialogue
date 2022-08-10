807fd1cb9a4a74373ceb2083c5fad08d2bd03e2c
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 14:07:35 +0430

    TestContactsRepository: Implement getContact

--- core-testing/src/main/java/io/github/zohrevand/dialogue/core/testing/repository/TestContactsRepository.kt
+++ core-testing/src/main/java/io/github/zohrevand/dialogue/core/testing/repository/TestContactsRepository.kt
@@ -5,6 +5,7 @@ import io.github.zohrevand.dialogue.core.data.repository.ContactsRepository
 import kotlinx.coroutines.channels.BufferOverflow.DROP_OLDEST
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.map
 
 class TestContactsRepository : ContactsRepository {
 
@@ -14,9 +15,8 @@ class TestContactsRepository : ContactsRepository {
     private val contactsFlow: MutableSharedFlow<List<Contact>> =
         MutableSharedFlow(replay = 1, onBufferOverflow = DROP_OLDEST)
 
-    override fun getContact(jid: String): Flow<Contact> {
-        TODO("Not yet implemented")
-    }
+    override fun getContact(jid: String): Flow<Contact> =
+        contactsFlow.map { contacts -> contacts.find { it.jid == jid }!! }
 
     override fun getContactsStream(): Flow<List<Contact>> = contactsFlow
 
