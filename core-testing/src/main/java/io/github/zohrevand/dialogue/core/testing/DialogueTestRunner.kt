facd6883dc4c357dc01414f8b8cc6540971dd95a
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 16:08:10 +0430

    Add DialogueTestRunner to set up instrumented application class for tests

---
+++ core-testing/src/main/java/io/github/zohrevand/dialogue/core/testing/DialogueTestRunner.kt
@@ -0,0 +1,15 @@
+package io.github.zohrevand.dialogue.core.testing
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+/**
+ * A custom runner to set up the instrumented application class for tests.
+ */
+class DialogueTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
