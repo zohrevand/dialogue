8eaefab88296f24aab0b62f7e2b7d1314d0e01d1
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 16:15:04 +0430

    Add testInstrumentationRunner to AndroidFeatureConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,6 +1,8 @@
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
@@ -11,6 +13,12 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
                 apply("org.jetbrains.kotlin.kapt")
+            }
+            extensions.configure<LibraryExtension> {
+                defaultConfig {
+                    testInstrumentationRunner =
+                        "io.github.zohrevand.dialogue.core.testing.DialogueTestRunner"
+                }
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
