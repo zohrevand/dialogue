12902eba22e9b9cb67cd96ddf53f42690c210d89
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 13:43:08 +0430

    Add a backing hot flow for list of contacts

--- core-testing/src/main/java/io/github/zohrevand/dialogue/core/testing/repository/TestContactsRepository.kt
+++ core-testing/src/main/java/io/github/zohrevand/dialogue/core/testing/repository/TestContactsRepository.kt
@@ -2,9 +2,17 @@ package io.github.zohrevand.dialogue.core.testing.repository
 
 import io.github.zohrevand.core.model.data.Contact
 import io.github.zohrevand.dialogue.core.data.repository.ContactsRepository
+import kotlinx.coroutines.channels.BufferOverflow.DROP_OLDEST
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 class TestContactsRepository : ContactsRepository {
+
+    /**
+     * The backing hot flow for the list of contacts for testing
+     */
+    private val contactsFlow: MutableSharedFlow<List<Contact>> =
+        MutableSharedFlow(replay = 1, onBufferOverflow = DROP_OLDEST)
 
     override fun getContact(jid: String): Flow<Contact> {
         TODO("Not yet implemented")
