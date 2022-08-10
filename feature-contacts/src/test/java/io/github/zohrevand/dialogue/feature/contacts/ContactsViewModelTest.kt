9b791f9b5f8617eca9e6dba36da26ea8bdc42774
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-10 14:17:36 +0430

    ContactsViewModelTest: test success ui state

--- feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
+++ feature-contacts/src/test/java/io/github/zohrevand/dialogue/feature/contacts/ContactsViewModelTest.kt
@@ -1,9 +1,15 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
+import io.github.zohrevand.core.model.data.Contact
 import io.github.zohrevand.dialogue.core.testing.repository.TestContactsRepository
 import io.github.zohrevand.dialogue.core.testing.util.MainDispatcherRule
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,4 +33,25 @@ class ContactsViewModelTest {
     fun uiStateContacts_whenInitialized_thenShowLoading() = runTest {
         assertEquals(ContactsUiState.Loading, viewModel.uiState.value)
     }
+
+    @Test
+    fun uiStateContacts_whenSuccess_matchesContactsFromRepository() = runTest {
+        val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+
+        contactsRepository.sendContacts(contacts.take(1))
+
+        val item = viewModel.uiState.value
+        assertTrue(item is ContactsUiState.Success)
+
+        val successContactsUiState = item as ContactsUiState.Success
+        val contact = contactsRepository.getContact(contacts[0].jid).first()
+        assertEquals(successContactsUiState.contacts, listOf(contact))
+
+        collectJob.cancel()
+    }
 }
+
+private val contacts = listOf(
+    Contact.create("hasan@dialogue.im"),
+    Contact.create("zohrevand@dialogue.im")
+)
