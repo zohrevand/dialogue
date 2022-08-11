beef54fd4c41aaa0c0916c5b451a42d2829aa446
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-11 23:53:18 +0430

    Jacoco coverageExclusions

---
+++ build-logic/convention/src/main/kotlin/io/github/zohrevand/dialogue/Jacoco.kt
@@ -0,0 +1,9 @@
+package io.github.zohrevand.dialogue
+
+private val coverageExclusions = listOf(
+    // Android
+    "**/R.class",
+    "**/R\$*.class",
+    "**/BuildConfig.*",
+    "**/Manifest*.*"
+)
