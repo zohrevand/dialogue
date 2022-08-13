c986fbf5b8b1cd27603d7d768068c5c5c09f4978
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-13 22:58:59 +0430

    Create new core-xmpp module

---
+++ core-xmpp/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("dialogue.android.library")
+    id("dialogue.android.library.jacoco")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    id("dialogue.spotless")
+}
+
+dependencies {
+    implementation(libs.smack.tcp)
+    api(libs.smack.android.extensions)
+
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // smack xpp3 exclusion
+    configurations {
+        all {
+            exclude(group = "xpp3", module = "xpp3")
+        }
+    }
+}
