2653cf104e78c028146cc6f168d040ea23f342b3
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-16 23:52:54 +0430

    Create a composition local for BackgroundTheme

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Background.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Background.kt
@@ -13,3 +13,8 @@ data class BackgroundTheme(
     val color: Color = Color.Unspecified,
     val tonalElevation: Dp = Dp.Unspecified
 )
+
+/**
+ * A composition local for [BackgroundTheme].
+ */
+val LocalBackgroundTheme = staticCompositionLocalOf { BackgroundTheme() }
