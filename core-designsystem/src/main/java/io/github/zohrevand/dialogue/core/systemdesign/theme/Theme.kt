4744653d7df5d3eea3787057a9d12b328e4605a2
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-16 23:55:36 +0430

    Define LightDefaultColorScheme

---
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Theme.kt
@@ -0,0 +1,33 @@
+package io.github.zohrevand.dialogue.core.systemdesign.theme
+
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Light default theme color scheme
+ */
+val LightDefaultColorScheme = lightColorScheme(
+    primary = Purple40,
+    onPrimary = Color.White,
+    primaryContainer = Purple90,
+    onPrimaryContainer = Purple10,
+    secondary = Orange40,
+    onSecondary = Color.White,
+    secondaryContainer = Orange90,
+    onSecondaryContainer = Orange10,
+    tertiary = Blue40,
+    onTertiary = Color.White,
+    tertiaryContainer = Blue90,
+    onTertiaryContainer = Blue10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = DarkPurpleGray99,
+    onBackground = DarkPurpleGray10,
+    surface = DarkPurpleGray99,
+    onSurface = DarkPurpleGray10,
+    surfaceVariant = PurpleGray90,
+    onSurfaceVariant = PurpleGray30,
+    outline = PurpleGray50
+)
