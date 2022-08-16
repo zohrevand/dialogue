3dfd9507b487c78f14755c906b4d13c1fc5ed30e
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-16 23:54:14 +0430

    Create model for gradient color

---
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Gradient.kt
@@ -0,0 +1,16 @@
+package io.github.zohrevand.dialogue.core.systemdesign.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+/**
+ * A class to model gradient color values for Dialogue.
+ */
+@Immutable
+data class GradientColors(
+    val primary: Color = Color.Unspecified,
+    val secondary: Color = Color.Unspecified,
+    val tertiary: Color = Color.Unspecified,
+    val neutral: Color = Color.Unspecified
+)
