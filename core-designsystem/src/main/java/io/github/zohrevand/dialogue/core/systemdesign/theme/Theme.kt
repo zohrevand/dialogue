282736f53361ae73636165d4b99bbea16f64bdae
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-16 23:58:34 +0430

    Define DarkAndroidBackgroundTheme

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Theme.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/theme/Theme.kt
@@ -135,3 +135,8 @@ val LightDefaultGradientColors = GradientColors(
  * Light Android background theme
  */
 val LightAndroidBackgroundTheme = BackgroundTheme(color = DarkGreenGray95)
+
+/**
+ * Dark Android background theme
+ */
+val DarkAndroidBackgroundTheme = BackgroundTheme(color = Color.Black)
