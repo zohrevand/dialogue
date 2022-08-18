31d92e02a9ff43513f111d80bdbb9c497d11344f
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-18 23:22:34 +0430

    Move AddContactDialog to a separate file

---
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/AddContactDialog.kt
@@ -0,0 +1,84 @@
+package io.github.zohrevand.dialogue.feature.contacts
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import io.github.zohrevand.dialogue.core.common.utils.isValidJid
+import io.github.zohrevand.dialogue.feature.contacts.R.string
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun AddContactDialog(
+    modifier: Modifier = Modifier,
+    onAddContact: (String) -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    val (newContact, setNewContact) = remember { mutableStateOf("") }
+    var contactError by remember { mutableStateOf(false) }
+
+    AlertDialog(
+        title = {
+            Text(text = stringResource(string.add_contact_title))
+        },
+        text = {
+            Column {
+                OutlinedTextField(
+                    value = newContact,
+                    onValueChange = {
+                        setNewContact(it)
+                        contactError = false
+                    },
+                    label = { Text(text = stringResource(string.new_contact)) },
+                    isError = contactError,
+                    modifier = Modifier.testTag("newContactTextField")
+                )
+                AnimatedVisibility(visible = contactError) {
+                    Text(
+                        text = stringResource(string.error_contact_is_not_valid_jabber_id),
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.labelSmall,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                }
+            }
+        },
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    contactError = !newContact.isValidJid
+                    if (!contactError) {
+                        onAddContact(newContact)
+                    }
+                },
+                modifier = Modifier.testTag("addContactButton")
+            ) {
+                Text(text = stringResource(string.add))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = stringResource(string.cancel))
+            }
+        },
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+        modifier = modifier
+    )
+}
--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
@@ -1,11 +1,9 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -27,15 +25,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,28 +38,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.zohrevand.core.model.data.Contact
-import io.github.zohrevand.dialogue.core.common.utils.isValidJid
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueDivider
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueLoadingWheel
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueTopAppBar
 import io.github.zohrevand.dialogue.feature.contacts.R.string.add
-import io.github.zohrevand.dialogue.feature.contacts.R.string.add_contact_title
-import io.github.zohrevand.dialogue.feature.contacts.R.string.cancel
 import io.github.zohrevand.dialogue.feature.contacts.R.string.contacts_title
-import io.github.zohrevand.dialogue.feature.contacts.R.string.error_contact_is_not_valid_jabber_id
-import io.github.zohrevand.dialogue.feature.contacts.R.string.new_contact
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
@@ -162,66 +149,6 @@ fun LazyListScope.contacts(
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-@Composable
-private fun AddContactDialog(
-    modifier: Modifier = Modifier,
-    onAddContact: (String) -> Unit,
-    onDismissRequest: () -> Unit
-) {
-    val (newContact, setNewContact) = remember { mutableStateOf("") }
-    var contactError by remember { mutableStateOf(false) }
-
-    AlertDialog(
-        title = {
-            Text(text = stringResource(add_contact_title))
-        },
-        text = {
-            Column {
-                OutlinedTextField(
-                    value = newContact,
-                    onValueChange = {
-                        setNewContact(it)
-                        contactError = false
-                    },
-                    label = { Text(text = stringResource(new_contact)) },
-                    isError = contactError,
-                    modifier = Modifier.testTag("newContactTextField")
-                )
-                AnimatedVisibility(visible = contactError) {
-                    Text(
-                        text = stringResource(error_contact_is_not_valid_jabber_id),
-                        color = MaterialTheme.colorScheme.error,
-                        style = MaterialTheme.typography.labelSmall,
-                        modifier = Modifier.padding(start = 16.dp)
-                    )
-                }
-            }
-        },
-        onDismissRequest = onDismissRequest,
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    contactError = !newContact.isValidJid
-                    if (!contactError) {
-                        onAddContact(newContact)
-                    }
-                },
-                modifier = Modifier.testTag("addContactButton")
-            ) {
-                Text(text = stringResource(add))
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismissRequest) {
-                Text(text = stringResource(cancel))
-            }
-        },
-        properties = DialogProperties(usePlatformDefaultWidth = false),
-        modifier = modifier
-    )
 }
 
 @Composable
