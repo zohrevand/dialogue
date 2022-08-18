d3fb740f103374fa230ec62a8b76d064c2e49cb0
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 02:28:07 +0430

    Add core-ui module to AndroidFeatureConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -28,6 +28,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", project(":core-data"))
                 add("implementation", project(":core-common"))
                 add("implementation", project(":core-designsystem"))
+                add("implementation", project(":core-ui"))
                 add("implementation", project(":core-navigation"))
 
                 add("testImplementation", project(":core-testing"))
