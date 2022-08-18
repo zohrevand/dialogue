c4f6c2c8d0760b6dcbe6dc6a25262b1de609da6d
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 02:45:18 +0430

    Use ContactThumb in ContactsScreen.kt

--- feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
+++ feature-contacts/src/main/java/io/github/zohrevand/dialogue/feature/contacts/ContactsScreen.kt
@@ -1,9 +1,7 @@
 package io.github.zohrevand.dialogue.feature.contacts
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -16,19 +14,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -39,7 +34,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -51,6 +45,7 @@ import io.github.zohrevand.core.model.data.firstLetter
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueDivider
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueLoadingWheel
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueTopAppBar
+import io.github.zohrevand.dialogue.core.ui.ContactThumb
 import io.github.zohrevand.dialogue.feature.contacts.R.string.add
 import io.github.zohrevand.dialogue.feature.contacts.R.string.contacts_title
 
@@ -164,18 +159,10 @@ private fun ContactItem(
             .height(80.dp)
             .clickable { onContactClick(contact.jid) }
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .size(56.dp)
-                .clip(CircleShape)
-                .background(Color.Cyan)
-        ) {
-            Text(
-                text = contact.firstLetter,
-                style = MaterialTheme.typography.titleMedium
-            )
-        }
+        ContactThumb(
+            firstLetter = contact.firstLetter,
+            color = Color.Cyan
+        )
 
         Text(text = contact.jid)
     }
