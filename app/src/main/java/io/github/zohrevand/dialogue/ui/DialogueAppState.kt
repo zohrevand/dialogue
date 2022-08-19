95447301f70b17d4c7747fadce35d1a6b0dec899
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 20:49:21 +0430

    Declare topLevelDestinations in DialogueAppState

--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
@@ -1,5 +1,8 @@
 package io.github.zohrevand.dialogue.ui
 
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
@@ -7,9 +10,13 @@ import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import io.github.zohrevand.dialogue.R.string
 import io.github.zohrevand.dialogue.feature.auth.navigation.AuthDestination
 import io.github.zohrevand.dialogue.feature.chat.navigation.ChatDestination
+import io.github.zohrevand.dialogue.feature.contacts.navigation.ContactsDestination
+import io.github.zohrevand.dialogue.feature.conversations.navigation.ConversationsDestination
 import io.github.zohrevand.dialogue.feature.router.navigation.RouterDestination
+import io.github.zohrevand.dialogue.navigation.TopLevelDestination
 
 @Composable
 fun rememberDialogueAppState(
@@ -28,9 +35,30 @@ class DialogueAppState(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val shouldShowBottomBar
+    val shouldShowBottomBar: Boolean
         @Composable get() = currentDestination?.route != null &&
             currentDestination?.route != RouterDestination.route &&
             currentDestination?.route != AuthDestination.route &&
             currentDestination?.route != ChatDestination.route
+
+    val shouldShowConnecting: Boolean
+        @Composable get() = currentDestination?.route != null &&
+            currentDestination?.route != RouterDestination.route &&
+            currentDestination?.route != AuthDestination.route
+
+    /**
+     * Top level destinations to be used in the BottomBar
+     */
+    val topLevelDestinations = listOf(
+        TopLevelDestination(
+            route = ConversationsDestination.route,
+            icon = Filled.Star,
+            iconTextId = string.conversations
+        ),
+        TopLevelDestination(
+            route = ContactsDestination.route,
+            icon = Filled.Person,
+            iconTextId = string.contacts
+        )
+    )
 }
