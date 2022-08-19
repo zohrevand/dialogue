f71460ecb43a0e328542a28e6d5b5eff2e0f52a3
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 20:54:51 +0430

    TopLevelDestination implements DialogueNavigationDestination

--- app/src/main/java/io/github/zohrevand/dialogue/navigation/DialogueTopLevelNavigation.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/navigation/DialogueTopLevelNavigation.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import io.github.zohrevand.dialogue.R
+import io.github.zohrevand.dialogue.core.navigation.DialogueNavigationDestination
 import io.github.zohrevand.dialogue.feature.contacts.navigation.ContactsDestination
 import io.github.zohrevand.dialogue.feature.conversations.navigation.ConversationsDestination
 
@@ -32,19 +33,22 @@ class DialogueTopLevelNavigation(private val navController: NavHostController) {
 }
 
 data class TopLevelDestination(
-    val route: String,
+    override val route: String,
+    override val destination: String,
     val icon: ImageVector,
     val iconTextId: Int
-)
+): DialogueNavigationDestination
 
 val TOP_LEVEL_DESTINATIONS = listOf(
     TopLevelDestination(
         route = ConversationsDestination.route,
+        destination = ConversationsDestination.destination,
         icon = Icons.Filled.Star,
         iconTextId = R.string.conversations
     ),
     TopLevelDestination(
         route = ContactsDestination.route,
+        destination = ContactsDestination.destination,
         icon = Icons.Filled.Person,
         iconTextId = R.string.contacts
     )
--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
@@ -7,10 +7,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import io.github.zohrevand.dialogue.R.string
+import io.github.zohrevand.dialogue.core.navigation.DialogueNavigationDestination
 import io.github.zohrevand.dialogue.feature.auth.navigation.AuthDestination
 import io.github.zohrevand.dialogue.feature.chat.navigation.ChatDestination
 import io.github.zohrevand.dialogue.feature.contacts.navigation.ContactsDestination
@@ -52,13 +54,48 @@ class DialogueAppState(
     val topLevelDestinations = listOf(
         TopLevelDestination(
             route = ConversationsDestination.route,
+            destination = ConversationsDestination.destination,
             icon = Filled.Star,
             iconTextId = string.conversations
         ),
         TopLevelDestination(
             route = ContactsDestination.route,
+            destination = ContactsDestination.destination,
             icon = Filled.Person,
             iconTextId = string.contacts
         )
     )
+
+    /**
+     * UI logic for navigating to a particular destination in the app. The NavigationOptions to
+     * navigate with are based on the type of destination, which could be a top level destination or
+     * just a regular destination.
+     *
+     * Top level destinations have only one copy of the destination of the back stack, and save and
+     * restore state whenever you navigate to and from it.
+     * Regular destinations can have multiple copies in the back stack and state isn't saved nor
+     * restored.
+     *
+     * @param destination: The [DialogueNavigationDestination] the app needs to navigate to.
+     * @param route: Optional route to navigate to in case the destination contains arguments.
+     */
+    fun navigate(destination: DialogueNavigationDestination, route: String? = null) {
+        if (destination is TopLevelDestination) {
+            navController.navigate(route ?: destination.route) {
+                // Pop up to the start destination of the graph to
+                // avoid building up a large stack of destinations
+                // on the back stack as users select items
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                // Avoid multiple copies of the same destination when
+                // reselecting the same item
+                launchSingleTop = true
+                // Restore state when reselecting a previously selected item
+                restoreState = true
+            }
+        } else {
+            navController.navigate(route ?: destination.route)
+        }
+    }
 }
