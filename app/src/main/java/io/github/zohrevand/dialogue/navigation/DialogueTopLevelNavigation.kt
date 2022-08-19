8450c18e74b0655231eb0af3c2bfbac62ded3995
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 20:56:02 +0430

    Add comment to TopLevelDestination

--- app/src/main/java/io/github/zohrevand/dialogue/navigation/DialogueTopLevelNavigation.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/navigation/DialogueTopLevelNavigation.kt
@@ -32,6 +32,11 @@ class DialogueTopLevelNavigation(private val navController: NavHostController) {
     }
 }
 
+/**
+ * Type for the top level destinations in the application. Each of these destinations
+ * can contain one or more screens (based on the window size). Navigation from one screen to the
+ * next within a single destination will be handled directly in composables.
+ */
 data class TopLevelDestination(
     override val route: String,
     override val destination: String,
