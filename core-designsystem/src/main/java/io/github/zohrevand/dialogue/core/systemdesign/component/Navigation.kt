03c98ff3766e67a6218edf602e7e3d7423259290
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 21:24:52 +0430

    Create DialogueNavigationBarItem

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/Navigation.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/Navigation.kt
@@ -4,10 +4,55 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
+/**
+ * Now in Android navigation bar item with icon and label content slots. Wraps Material 3
+ * [NavigationBarItem].
+ *
+ * @param selected Whether this item is selected.
+ * @param onClick The callback to be invoked when this item is selected.
+ * @param icon The item icon content.
+ * @param modifier Modifier to be applied to this item.
+ * @param selectedIcon The item icon content when selected.
+ * @param enabled controls the enabled state of this item. When `false`, this item will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param label The item text label content.
+ * @param alwaysShowLabel Whether to always show the label for this item. If false, the label will
+ * only be shown when this item is selected.
+ */
+@Composable
+fun RowScope.DialogueNavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    selectedIcon: @Composable () -> Unit = icon,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true
+) {
+    NavigationBarItem(
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        modifier = modifier,
+        enabled = enabled,
+        label = label,
+        alwaysShowLabel = alwaysShowLabel,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = DialogueNavigationDefaults.navigationSelectedItemColor(),
+            unselectedIconColor = DialogueNavigationDefaults.navigationContentColor(),
+            selectedTextColor = DialogueNavigationDefaults.navigationSelectedItemColor(),
+            unselectedTextColor = DialogueNavigationDefaults.navigationContentColor(),
+            indicatorColor = DialogueNavigationDefaults.navigationIndicatorColor()
+        )
+    )
+}
 
 /**
  * Dialogue navigation bar with content slot. Wraps Material 3 [NavigationBar].
@@ -37,4 +82,8 @@ object DialogueNavigationDefaults {
     val NavigationContainerColor = Color.Transparent
     @Composable
     fun navigationContentColor() = MaterialTheme.colorScheme.onSurfaceVariant
+    @Composable
+    fun navigationSelectedItemColor() = MaterialTheme.colorScheme.onPrimaryContainer
+    @Composable
+    fun navigationIndicatorColor() = MaterialTheme.colorScheme.primaryContainer
 }
