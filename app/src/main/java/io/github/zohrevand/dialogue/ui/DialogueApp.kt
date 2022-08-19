1a18bf236d86393f7271542a5e569444b65f20ba
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 21:26:17 +0430

    Remove unused imports

--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueApp.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueApp.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
