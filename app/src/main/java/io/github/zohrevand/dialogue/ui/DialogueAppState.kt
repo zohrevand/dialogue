5e97698bb1b9ddc0d9bacadb3e8d7316e24c76be
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 20:37:21 +0430

    Create DialogueAppState

---
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
@@ -0,0 +1,10 @@
+package io.github.zohrevand.dialogue.ui
+
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavHostController
+
+@Stable
+class DialogueAppState(
+    val navController: NavHostController
+) {
+}
