a06455e6a2a5017ccddb3c93ef94f648c822d878
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-19 20:38:45 +0430

    Create rememberDialogueAppState

--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueAppState.kt
@@ -1,7 +1,19 @@
 package io.github.zohrevand.dialogue.ui
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun rememberDialogueAppState(
+    navController: NavHostController = rememberNavController()
+): DialogueAppState {
+    return remember(navController) {
+        DialogueAppState(navController)
+    }
+}
 
 @Stable
 class DialogueAppState(
