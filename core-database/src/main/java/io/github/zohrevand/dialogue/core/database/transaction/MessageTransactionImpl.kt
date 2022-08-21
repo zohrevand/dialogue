d3fafcd12a60eed3db441bb0919ad03d028ace4b
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-21 12:25:57 +0430

    Make handleIncomingMessage synchronized with mutex

--- core-database/src/main/java/io/github/zohrevand/dialogue/core/database/transaction/MessageTransactionImpl.kt
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/transaction/MessageTransactionImpl.kt
@@ -7,36 +7,43 @@ import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
 import io.github.zohrevand.dialogue.core.database.model.MessageEntity
 import javax.inject.Inject
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class MessageTransactionImpl @Inject constructor(
     private val database: DialogueDatabase
 ) : MessageTransaction {
+
+    private val mutex = Mutex()
+
     override suspend fun handleIncomingMessage(
         messageEntity: MessageEntity,
         maybeNewConversationEntity: ConversationEntity
-    ) = database.withTransaction {
-        val conversationDao = database.conversationDao()
-        val messageDao = database.messageDao()
+    ) = mutex.withLock {
+        database.withTransaction {
+            val conversationDao = database.conversationDao()
+            val messageDao = database.messageDao()
 
-        val peerJid = messageEntity.peerJid
-        val conversation = conversationDao.getConversationEntity(peerJid).first()
+            val peerJid = messageEntity.peerJid
+            val conversation = conversationDao.getConversationEntity(peerJid).first()
 
-        if (conversation == null) {
-            conversationDao.insert(maybeNewConversationEntity)
+            if (conversation == null) {
+                conversationDao.insert(maybeNewConversationEntity)
+            }
+
+            val messageId = messageDao.insert(messageEntity)
+
+            val unreadMessagesCount =
+                if (conversation == null) 1
+                else if (conversation.entity.isChatOpen) 0
+                else conversation.entity.unreadMessagesCount + 1
+
+            conversationDao.update(
+                peerJid = peerJid,
+                unreadMessagesCount = unreadMessagesCount,
+                chatState = ChatState.Active,
+                lastMessageId = messageId
+            )
         }
-
-        val messageId = messageDao.insert(messageEntity)
-
-        val unreadMessagesCount =
-            if (conversation == null) 1
-            else if (conversation.entity.isChatOpen) 0
-            else conversation.entity.unreadMessagesCount + 1
-
-        conversationDao.update(
-            peerJid = peerJid,
-            unreadMessagesCount = unreadMessagesCount,
-            chatState = ChatState.Active,
-            lastMessageId = messageId
-        )
     }
 }
