84b3504bdb6d22c860892686cd7fcc5ab5e1b60a
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-21 12:12:23 +0430

    Implement handleIncomingMessage

---
+++ core-database/src/main/java/io/github/zohrevand/dialogue/core/database/transaction/MessageTransactionImpl.kt
@@ -0,0 +1,42 @@
+package io.github.zohrevand.dialogue.core.database.transaction
+
+import androidx.room.withTransaction
+import io.github.zohrevand.core.model.data.ChatState
+import io.github.zohrevand.dialogue.core.database.DialogueDatabase
+import io.github.zohrevand.dialogue.core.database.model.ConversationEntity
+import io.github.zohrevand.dialogue.core.database.model.MessageEntity
+import javax.inject.Inject
+import kotlinx.coroutines.flow.first
+
+class MessageTransactionImpl @Inject constructor(
+    private val database: DialogueDatabase
+) : MessageTransaction {
+    override suspend fun handleIncomingMessage(
+        messageEntity: MessageEntity,
+        maybeNewConversationEntity: ConversationEntity
+    ) = database.withTransaction {
+        val conversationDao = database.conversationDao()
+        val messageDao = database.messageDao()
+
+        val peerJid = messageEntity.peerJid
+        val conversation = conversationDao.getConversationEntity(peerJid).first()
+
+        if (conversation == null) {
+            conversationDao.insert(maybeNewConversationEntity)
+        }
+
+        val messageId = messageDao.insert(messageEntity)
+
+        val unreadMessagesCount =
+            if (conversation == null) 1
+            else if (conversation.entity.isChatOpen) 0
+            else conversation.entity.unreadMessagesCount + 1
+
+        conversationDao.update(
+            peerJid = peerJid,
+            unreadMessagesCount = unreadMessagesCount,
+            chatState = ChatState.Active,
+            lastMessageId = messageId
+        )
+    }
+}
