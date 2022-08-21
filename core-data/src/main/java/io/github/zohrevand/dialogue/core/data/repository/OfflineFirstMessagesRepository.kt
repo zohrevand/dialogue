7c2aca36fab3cf53a9a1e9afa8b773c049e73129
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-21 12:13:15 +0430

    Add messageTransaction to MessageRepository

--- core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstMessagesRepository.kt
+++ core-data/src/main/java/io/github/zohrevand/dialogue/core/data/repository/OfflineFirstMessagesRepository.kt
@@ -1,17 +1,20 @@
 package io.github.zohrevand.dialogue.core.data.repository
 
+import io.github.zohrevand.core.model.data.Conversation
 import io.github.zohrevand.core.model.data.Message
 import io.github.zohrevand.core.model.data.MessageStatus
 import io.github.zohrevand.dialogue.core.data.model.asEntity
 import io.github.zohrevand.dialogue.core.database.dao.MessageDao
 import io.github.zohrevand.dialogue.core.database.model.MessageEntity
 import io.github.zohrevand.dialogue.core.database.model.asExternalModel
+import io.github.zohrevand.dialogue.core.database.transaction.MessageTransaction
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class OfflineFirstMessagesRepository @Inject constructor(
-    private val messageDao: MessageDao
+    private val messageDao: MessageDao,
+    private val messageTransaction: MessageTransaction
 ) : MessagesRepository {
 
     override fun getMessage(id: String): Flow<Message> =
@@ -47,4 +50,12 @@ class OfflineFirstMessagesRepository @Inject constructor(
 
     override suspend fun deleteMessage(id: String) =
         messageDao.deleteMessage(id)
+
+    override suspend fun handleIncomingMessage(
+        message: Message,
+        maybeNewConversation: Conversation
+    ) = messageTransaction.handleIncomingMessage(
+        message.asEntity(),
+        maybeNewConversation.asEntity()
+    )
 }
