3eb974f4633277801a030fbeb0835f7818120c55
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-22 03:16:49 +0430

    Create KeyboardState

---
+++ feature-chat/src/main/java/io/github/zohrevand/dialogue/feature/chat/KeyboardState.kt
@@ -0,0 +1,36 @@
+package io.github.zohrevand.dialogue.feature.chat
+
+import android.graphics.Rect
+import android.view.ViewTreeObserver
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalView
+
+enum class KeyboardState {
+    Opened, Closed
+}
+
+@Composable
+fun keyboardAsState(): State<KeyboardState> {
+    val keyboardState = remember { mutableStateOf(KeyboardState.Closed) }
+    val view = LocalView.current
+    DisposableEffect(view) {
+        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
+            val rect = Rect()
+            view.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = view.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+            keyboardState.value = if (keypadHeight > screenHeight * 0.15) {
+                KeyboardState.Opened
+            } else {
+                KeyboardState.Closed
+            }
+        }
+        view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
+        }
+    }
+
+    return keyboardState
+}
