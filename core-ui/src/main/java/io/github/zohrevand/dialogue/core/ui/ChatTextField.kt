d3b96d6c24d22607944b976deb4c07e431fa8925
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-22 03:12:14 +0430

    Make ChatTextField conform to material theme

--- core-ui/src/main/java/io/github/zohrevand/dialogue/core/ui/ChatTextField.kt
+++ core-ui/src/main/java/io/github/zohrevand/dialogue/core/ui/ChatTextField.kt
@@ -1,16 +1,14 @@
 package io.github.zohrevand.dialogue.core.ui
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueOutlinedTextField
@@ -20,23 +18,20 @@ import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueOutlined
 fun ChatTextField(
     value: String,
     onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    @StringRes placeholderTextId: Int
+    modifier: Modifier = Modifier
 ) {
     DialogueOutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        placeholder = { Text(text = stringResource(placeholderTextId)) },
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
         maxLines = 4,
         colors = TextFieldDefaults.outlinedTextFieldColors(
-            containerColor = Color.Gray,
-            focusedBorderColor = Color.DarkGray,
-            unfocusedBorderColor = Color.Transparent,
-            cursorColor = Color.DarkGray
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent
         ),
         shape = RoundedCornerShape(24.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp),
         modifier = modifier
     )
 }
