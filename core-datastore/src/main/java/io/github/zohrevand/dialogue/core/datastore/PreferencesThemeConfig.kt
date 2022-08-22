c4f0b4fae454d77dbcf61964d9caa85ea2dff91d
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-22 18:36:01 +0430

    Mapper form PreferencesThemeConfig to ThemeConfig

--- core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/PreferencesThemeConfig.kt
+++ core-datastore/src/main/java/io/github/zohrevand/dialogue/core/datastore/PreferencesThemeConfig.kt
@@ -1,9 +1,15 @@
 package io.github.zohrevand.dialogue.core.datastore
 
+import io.github.zohrevand.core.model.data.ThemeConfig
 import io.github.zohrevand.dialogue.core.datastore.UserPreferences.DarkConfig
 import io.github.zohrevand.dialogue.core.datastore.UserPreferences.ThemeBranding
 
 data class PreferencesThemeConfig(
     val themeBranding: ThemeBranding,
     val darkConfig: DarkConfig
+)
+
+fun PreferencesThemeConfig.asExternalModel() = ThemeConfig(
+    themeBranding = io.github.zohrevand.core.model.data.ThemeBranding.valueOf(themeBranding.name),
+    darkConfig = io.github.zohrevand.core.model.data.DarkConfig.valueOf(darkConfig.name)
 )
