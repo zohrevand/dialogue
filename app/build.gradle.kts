676b560eb52b04a57a85985e0ca415ff254e6b00
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-23 00:07:44 +0430

    Add accompanist systemuicontroller to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtimeCompose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.accompanist.systemuicontroller)
 
     api(libs.androidx.hilt.navigation.compose)
     api(libs.androidx.navigation.compose)
