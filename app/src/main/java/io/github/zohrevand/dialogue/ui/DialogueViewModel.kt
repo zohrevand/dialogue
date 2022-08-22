e310e60a86b430ce90c12c7199989463933e6904
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-22 20:18:23 +0430

    Refactor uiState logic

--- app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueViewModel.kt
+++ app/src/main/java/io/github/zohrevand/dialogue/ui/DialogueViewModel.kt
@@ -12,38 +12,34 @@ import io.github.zohrevand.dialogue.ui.ConnectionUiState.Connected
 import io.github.zohrevand.dialogue.ui.ConnectionUiState.Connecting
 import io.github.zohrevand.dialogue.ui.ConnectionUiState.Idle
 import javax.inject.Inject
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class DialogueViewModel @Inject constructor(
-    private val preferencesRepository: PreferencesRepository,
+    preferencesRepository: PreferencesRepository,
     private val conversationsRepository: ConversationsRepository,
     private val sendingChatStatesRepository: SendingChatStatesRepository
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<ConnectionUiState> = MutableStateFlow(Idle)
-    val uiState: StateFlow<ConnectionUiState> = _uiState.asStateFlow()
-
-    init {
-        viewModelScope.launch {
-            observeConnectionStatus()
-        }
-    }
-
-    private suspend fun observeConnectionStatus() {
-        preferencesRepository.getConnectionStatus().collect { connectionStatus ->
-            if (connectionStatus.availability && connectionStatus.authenticated) {
-                _uiState.update { Connected }
-            } else {
-                // TODO: navigate to auth screen if connection available but not authenticated
-                _uiState.update { Connecting }
+    val uiState: StateFlow<ConnectionUiState> =
+        preferencesRepository.getConnectionStatus()
+            .map { connectionStatus ->
+                if (connectionStatus.availability && connectionStatus.authenticated) {
+                    Connected
+                } else {
+                    // TODO: navigate to auth screen if connection available but not authenticated
+                    Connecting
+                }
             }
-        }
-    }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = Idle
+            )
 
     fun onExitChat(contactId: String) {
         viewModelScope.launch {
