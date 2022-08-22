df727f9525fe22ab1fafbe3c566e835eb606ce3d
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-22 19:54:43 +0430

    Config theme in SettingsScreen

--- feature-settings/src/main/java/io/github/zohrevand/dialogue/feature/settings/SettingsScreen.kt
+++ feature-settings/src/main/java/io/github/zohrevand/dialogue/feature/settings/SettingsScreen.kt
@@ -1,22 +1,197 @@
 package io.github.zohrevand.dialogue.feature.settings
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.github.zohrevand.core.model.data.DarkConfig
+import io.github.zohrevand.core.model.data.ThemeBranding
+import io.github.zohrevand.core.model.data.ThemeConfig
+import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueDivider
+import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueLoadingWheel
+import io.github.zohrevand.dialogue.core.systemdesign.component.DialogueTopAppBar
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
-fun SettingsRoute() {
-    SettingsScreen()
+fun SettingsRoute(
+    viewModel: SettingsViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    SettingsScreen(
+        uiState = uiState,
+        onThemeBrandingSelect = viewModel::selectThemeBranding,
+        onDarkConfigSelect = viewModel::selectDarkConfig
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen(
+    uiState: SettingsUiState,
+    onThemeBrandingSelect: (ThemeBranding) -> Unit,
+    onDarkConfigSelect: (DarkConfig) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            DialogueTopAppBar(
+                titleRes = R.string.settings,
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier.windowInsetsPadding(
+                    WindowInsets.safeDrawing.only(WindowInsetsSides.Top)
+                )
+            )
+        },
+        containerColor = Color.Transparent,
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            when (uiState) {
+                is SettingsUiState.Loading -> {
+                    DialogueLoadingWheel(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentSize()
+                    )
+                }
+                is SettingsUiState.Success -> {
+                    ThemeBranding(
+                        title = "Theme Branding",
+                        themeConfig = uiState.themeConfig,
+                        themeBrandings = uiState.themeBrandings,
+                        onThemeBrandingSelect = onThemeBrandingSelect
+                    )
+
+                    DarkConfig(
+                        title = "Dark Config",
+                        themeConfig = uiState.themeConfig,
+                        darkConfigs = uiState.darkConfigs,
+                        onDarkConfigSelect = onDarkConfigSelect
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
-fun SettingsScreen() {
+private fun ThemeBranding(
+    title: String,
+    themeConfig: ThemeConfig,
+    themeBrandings: List<ThemeBranding>,
+    onThemeBrandingSelect: (ThemeBranding) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        LazyColumn {
+            items(themeBrandings, key = { it.name }) { themeBranding ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Text(
+                        text = themeBranding.name,
+                        modifier = Modifier
+                    )
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Magenta))
+                    RadioButton(
+                        selected = themeConfig.themeBranding == themeBranding,
+                        onClick = { onThemeBrandingSelect(themeBranding) }
+                    )
+                }
+
+                DialogueDivider()
+            }
+        }
+    }
+}
+
+@Composable
+private fun DarkConfig(
+    title: String,
+    themeConfig: ThemeConfig,
+    darkConfigs: List<DarkConfig>,
+    onDarkConfigSelect: (DarkConfig) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        LazyColumn {
+            items(darkConfigs, key = { it.name }) { darkConfig ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Text(
+                        text = darkConfig.name,
+                        modifier = Modifier
+                    )
+
+                    RadioButton(
+                        selected = themeConfig.darkConfig == darkConfig,
+                        onClick = { onDarkConfigSelect(darkConfig) }
+                    )
+                }
+
+                DialogueDivider()
+            }
+        }
+    }
 }
--- feature-settings/src/main/java/io/github/zohrevand/dialogue/feature/settings/SettingsViewModel.kt
+++ feature-settings/src/main/java/io/github/zohrevand/dialogue/feature/settings/SettingsViewModel.kt
@@ -3,6 +3,8 @@ package io.github.zohrevand.dialogue.feature.settings
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.zohrevand.core.model.data.DarkConfig
+import io.github.zohrevand.core.model.data.ThemeBranding
 import io.github.zohrevand.core.model.data.ThemeConfig
 import io.github.zohrevand.dialogue.core.data.repository.PreferencesRepository
 import javax.inject.Inject
@@ -10,26 +12,59 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    preferencesRepository: PreferencesRepository
+    private val preferencesRepository: PreferencesRepository
 ) : ViewModel() {
 
     val uiState: StateFlow<SettingsUiState> =
         preferencesRepository.getThemeConfig()
             .map { themeConfig ->
-                SettingsUiState.Success(themeConfig)
+                SettingsUiState.Success(
+                    themeConfig = themeConfig,
+                    themeBrandings = ThemeBranding.values().toList(),
+                    darkConfigs = DarkConfig.values().toList()
+                )
             }
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(5_000),
                 initialValue = SettingsUiState.Loading
             )
+
+    fun selectThemeBranding(themeBranding: ThemeBranding) {
+        if (uiState.value is SettingsUiState.Success) {
+            val themeConfig = (uiState.value as SettingsUiState.Success).themeConfig
+
+            viewModelScope.launch {
+                preferencesRepository.updateThemeConfig(
+                    themeConfig.copy(themeBranding = themeBranding)
+                )
+            }
+        }
+    }
+
+    fun selectDarkConfig(darkConfig: DarkConfig) {
+        if (uiState.value is SettingsUiState.Success) {
+            val themeConfig = (uiState.value as SettingsUiState.Success).themeConfig
+
+            viewModelScope.launch {
+                preferencesRepository.updateThemeConfig(
+                    themeConfig.copy(darkConfig = darkConfig)
+                )
+            }
+        }
+    }
 }
 
 sealed interface SettingsUiState {
     object Loading : SettingsUiState
 
-    data class Success(val themeConfig: ThemeConfig) : SettingsUiState
+    data class Success(
+        val themeConfig: ThemeConfig,
+        val themeBrandings: List<ThemeBranding>,
+        val darkConfigs: List<DarkConfig>
+    ) : SettingsUiState
 }
