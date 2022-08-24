c52e8b17ac39f0f3f2a21310a89c5d9fbcc18b93
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-24 14:37:37 +0430

    Refactor datetime utilities to DateTimeUtils

---
+++ core-common/src/main/java/io/github/zohrevand/dialogue/core/common/utils/DateTimeUtils.kt
@@ -0,0 +1,52 @@
+package io.github.zohrevand.dialogue.core.common.utils
+
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.toJavaInstant
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toLocalDateTime
+
+val Instant.formatted: String
+    get() = when (this.localDate) {
+        today -> localTime
+        yesterday -> "Yesterday"
+        else -> dateFormatted
+    }
+
+val LocalDate.formatted: String
+    get() = when (this) {
+        today -> "Today"
+        yesterday -> "Yesterday"
+        else -> dateFormatted
+    }
+
+val Instant.localTime: String
+    get() = "${localDateTime.hour}:${localDateTime.minute}"
+
+val Instant.localDate: LocalDate
+    get() = localDateTime.date
+
+private val Instant.localDateTime: LocalDateTime
+    get() = toLocalDateTime(TimeZone.currentSystemDefault())
+
+private val Instant.dateFormatted: String
+    get() = dateTimeFormatter.withZone(ZoneId.systemDefault()).format(this.toJavaInstant())
+
+private val LocalDate.dateFormatted: String
+    get() = toJavaLocalDate().format(dateTimeFormatter)
+
+private val today: LocalDate
+    get() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+
+private val yesterday: LocalDate
+    get() = today.minus(1, DateTimeUnit.DAY)
+
+private val dateTimeFormatter: DateTimeFormatter
+    get() = DateTimeFormatter.ofPattern("M/d/yyyy")
