fa963afb35bd06a3d4797ca36195a4563c1eaa19
Hasan Zohrevand <zohrevan@gmail.com>
2022-08-24 15:11:24 +0430

    Allow top app bar with not centered title

--- core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
+++ core-designsystem/src/main/java/io/github/zohrevand/dialogue/core/systemdesign/component/TopAppBar.kt
@@ -1,11 +1,13 @@
 package io.github.zohrevand.dialogue.core.systemdesign.component
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
@@ -43,6 +45,7 @@ fun DialogueTopAppBar(
 @Composable
 fun DialogueTopAppBar(
     modifier: Modifier = Modifier,
+    centeredTitle: Boolean = true,
     title: @Composable () -> Unit,
     navigationIcon: ImageVector? = null,
     navigationIconContentDescription: String? = null,
@@ -52,31 +55,44 @@ fun DialogueTopAppBar(
     onNavigationClick: () -> Unit = {},
     onActionClick: () -> Unit = {}
 ) {
-    CenterAlignedTopAppBar(
-        title = title,
-        navigationIcon = {
-            navigationIcon?.let {
-                IconButton(onClick = onNavigationClick) {
-                    Icon(
-                        imageVector = navigationIcon,
-                        contentDescription = navigationIconContentDescription,
-                        tint = MaterialTheme.colorScheme.onSurface
-                    )
-                }
+    val navIcon: @Composable () -> Unit = {
+        navigationIcon?.let {
+            IconButton(onClick = onNavigationClick) {
+                Icon(
+                    imageVector = navigationIcon,
+                    contentDescription = navigationIconContentDescription,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
             }
-        },
-        actions = {
-            actionIcon?.let {
-                IconButton(onClick = onActionClick) {
-                    Icon(
-                        imageVector = actionIcon,
-                        contentDescription = actionIconContentDescription,
-                        tint = MaterialTheme.colorScheme.onSurface
-                    )
-                }
+        }
+    }
+    val actions: @Composable (RowScope.() -> Unit) = {
+        actionIcon?.let {
+            IconButton(onClick = onActionClick) {
+                Icon(
+                    imageVector = actionIcon,
+                    contentDescription = actionIconContentDescription,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
             }
-        },
-        colors = colors,
-        modifier = modifier
-    )
+        }
+    }
+
+    if (centeredTitle) {
+        CenterAlignedTopAppBar(
+            title = title,
+            navigationIcon = navIcon,
+            actions = actions,
+            colors = colors,
+            modifier = modifier
+        )
+    } else {
+        SmallTopAppBar(
+            title = title,
+            navigationIcon = navIcon,
+            actions = actions,
+            colors = colors,
+            modifier = modifier
+        )
+    }
 }
